486e03266d7ba537d2bed080fef46869c78441cd
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2013-08-30 18:24:13 +0200

    prework railroad formats

---
+++ serializer-poc/src/railroad_formats.h
@@ -0,0 +1,9 @@
+#ifndef __RAILROAD_FORMATS_H__
+#define __RAILROAD_FORMATS_H__
+
+/* DCC must be stretched 20*58 > 1024 (DIVINT 9:0)*/
+#define DCC_PERIOD	58
+#define MM_PERIOD	13
+#define MFX_PERIOD	50
+
+#endif //__RAILROAD_FORMATS_H__
