495653ed48958bc1ee4e3301fc6ab68ffb4c6641
GBert <info@gerhard-bertelsmann.de>
2014-12-15 12:04:03 +0100

    put defines into header file

---
+++ can2udp/src/can2lan.h
@@ -0,0 +1,54 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <info@gerhard-bertelsmann.de> wrote this file. As long as you retain this
+ * notice you can do whatever you want with this stuff. If we meet some day,
+ * and you think this stuff is worth it, you can buy me a beer in return
+ * Gerhard Bertelsmann
+ * ----------------------------------------------------------------------------
+ */
+
+/* Thanks to Stefan Krauss and the SocketCAN team
+ */
+#ifndef _CAN2LAN_H_
+#define _CAN2LAN_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <libgen.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <ctype.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <time.h>
+#include <linux/can.h>
+
+#define MAX_TCP_CONN	16		/* max TCP clients */
+#define MAXDG   	4096		/* maximum datagram size */
+#define MAXUDP  	16		/* maximum datagram size */
+#define MAX(a,b)	((a) > (b) ? (a) : (b))
+#define debug_print(...) \
+            do { if (DEBUG) fprintf(stderr, ##__VA_ARGS__); } while (0)
+
+char *CAN_FORMAT_STRG       ="      CAN->  CANID 0x%06X R [%d]";
+char *TO_CAN_FORMAT_STRG    ="      CAN    CANID 0x%06X   [%d]";
+char *UDP_FORMAT_STRG       ="->CAN>UDP    CANID 0x%06X   [%d]";
+char *TCP_FORMAT_STRG       ="->TCP>CAN    CANID 0x%06X   [%d]";
+char *CAN_TCP_FORMAT_STRG   ="->CAN>TCP    CANID 0x%06X   [%d]";
+char *NET_UDP_FORMAT_STRG   ="      UDP->  CANID 0x%06X   [%d]";
+
+unsigned char M_GLEISBOX_MAGIC_START_SEQUENCE [] = {0x00,0x36,0x03,0x01,0x05,0x00,0x00,0x00,0x00,0x11,0x00,0x00,0x00};
+
+static const int MAXPENDING = 16;	/* max outstanding tcp connections */
+unsigned char netframe[MAXDG];
+#endif /* _CAN2LAN_H_ */
+
