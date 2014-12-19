de5ec3748210316b8c6f0eadc5385fd41c540594
GBert <info@gerhard-bertelsmann.de>
2014-12-19 13:47:39 +0100

    add missing setup instruction

--- can2udp/src/virtual_gfp.c
+++ can2udp/src/virtual_gfp.c
@@ -28,6 +28,8 @@
   sudo modprobe can-gw
   sudo ip link add dev vcan0 type vcan
   sudo ip link add dev vcan1 type vcan
+  sudo ifconfig vcan0 up
+  sudo ifconfig vcan1 up
   sudo cangw -A -s vcan0 -d vcan1 -e
   sudo cangw -A -s vcan1 -d vcan0 -e
   ./virtual_gfp -f -i vcan1
