ea8fe041ef002a792da7b741795b55b8af2ac647
GBert <info@gerhard-bertelsmann.de>
2014-12-23 17:23:59 +0100

    beautify the code and check return code of connect

--- can2udp/src/ask-gbs0.c
+++ can2udp/src/ask-gbs0.c
@@ -1,5 +1,13 @@
-#include "can2lan.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <time.h>
 
 unsigned char GBS0[]   = {0x00,0x40,0x03,0x00,0x08,0x67,0x62,0x73,0x2d,0x30,0x00,0x00,0x00};
 unsigned char GBSSTAT[]= {0x00,0x40,0x03,0x00,0x08,0x67,0x62,0x73,0x73,0x74,0x61,0x74,0x00};
@@ -14,37 +22,39 @@ int netframe_to_net(int net_socket, unsigned char *netframe, int length) {
     return 0;
 }
 
-int main(int argc, char**argv)
-{
-   int sockfd, i, n=1;
-   struct sockaddr_in servaddr;
-   unsigned char recvline[1000];
+int main(int argc, char**argv) {
+    int sockfd, i, n=1;
+    struct sockaddr_in servaddr;
+    unsigned char recvline[1000];
 
-   if (argc != 2)
-   {
-      printf("usage:  client <IP address>\n");
-      exit(1);
-   }
+    if (argc != 2)
+    {
+       printf("usage:  client <IP address>\n");
+       exit(1);
+    }
 
-   sockfd=socket(AF_INET,SOCK_STREAM,0);
+    sockfd=socket(AF_INET,SOCK_STREAM,0);
 
-   bzero(&servaddr,sizeof(servaddr));
-   servaddr.sin_family = AF_INET;
-   servaddr.sin_addr.s_addr=inet_addr(argv[1]);
-   servaddr.sin_port=htons(15731);
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr=inet_addr(argv[1]);
+    servaddr.sin_port=htons(15731);
 
-   connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-   /* netframe_to_net(sockfd, GBS0, 13); */
-   netframe_to_net(sockfd, GBSSTAT, 13);
-   while(n) {
-      n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
-      for ( i=0; i<n; i++) {
-          if (( i % 13 ) == 0) {
-              printf("\n");
-          }
-          printf("%02x ", recvline[i]);
-      }
-      printf("\n");
-   }
-   return 1;
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) {
+        printf("can't connect to socket - terminating\n");
+        return -1;
+    }
+    /* netframe_to_net(sockfd, GBS0, 13); */
+    netframe_to_net(sockfd, GBSSTAT, 13);
+    while(n) {
+        n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
+        for ( i=0; i<n; i++) {
+            if (( i % 13 ) == 0) {
+                printf("\n");
+            }
+            printf("%02x ", recvline[i]);
+        }
+        printf("\n");
+    }
+    return 1;
 }
