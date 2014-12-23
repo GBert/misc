613750506679df666a217d4a33b379b48a8594d9
Gerhard Bertelsmann <gerhard.bertelsmann@googlemail.com>
2014-12-24 00:25:02 +0100

    make the config read tool more generic

--- can2udp/src/get-cs-config.c
+++ can2udp/src/get-cs-config.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <strings.h>
 #include <errno.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
-#include <time.h>
 
-unsigned char GBS0[]   = {0x00,0x40,0x03,0x00,0x08,0x67,0x62,0x73,0x2d,0x30,0x00,0x00,0x00};
-unsigned char GBSSTAT[]= {0x00,0x40,0x03,0x00,0x08,0x67,0x62,0x73,0x73,0x74,0x61,0x74,0x00};
+#define FRAME_SIZE	13
+unsigned char GETCONFIG[] = {0x00,0x40,0x03,0x00,0x08};
 
 int netframe_to_net(int net_socket, unsigned char *netframe, int length) {
     int s;
@@ -25,27 +24,44 @@ int netframe_to_net(int net_socket, unsigned char *netframe, int length) {
 int main(int argc, char**argv) {
     int sockfd, i, n=1;
     struct sockaddr_in servaddr;
+    unsigned char netframe[FRAME_SIZE];
     unsigned char recvline[1000];
-
-    if (argc != 2)
+    
+    if (argc != 3)
     {
-       printf("usage:  client <IP address>\n");
+       printf("usage:  %s <config> <IP address>\n", argv[0]);
        exit(1);
     }
 
-    sockfd=socket(AF_INET,SOCK_STREAM,0);
+    if (strlen(argv[1])>7) {
+        printf("config name to long\n");
+        exit(1);
+    }
+
+    if((sockfd=socket(AF_INET,SOCK_STREAM,0)) < 0) {
+        printf("can't crate TCP socket\n");
+        exit(1);
+    }
 
     bzero(&servaddr,sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr=inet_addr(argv[1]);
+    if (inet_aton((argv[2]), (struct in_addr *) &servaddr.sin_addr.s_addr) == 0) {
+        printf("invalid address\n");
+        exit(1);
+    }
+    
     servaddr.sin_port=htons(15731);
 
     if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) {
         printf("can't connect to socket - terminating\n");
         exit(1);
     }
-    /* netframe_to_net(sockfd, GBS0, 13); */
-    netframe_to_net(sockfd, GBSSTAT, 13);
+
+    bzero(netframe, sizeof(netframe));
+    memcpy(netframe, GETCONFIG,5);
+    memcpy(&netframe[5], argv[1], strlen(argv[1]));
+    netframe_to_net(sockfd, netframe, FRAME_SIZE);
+
     while(n) {
         n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
         for ( i=0; i<n; i++) {
