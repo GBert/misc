d11963e5e858f4aa2141426dabd85bb12948092a
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2014-12-24 15:25:16 +0100

    use blocking socket

--- can2udp/src/get-cs-config.c
+++ can2udp/src/get-cs-config.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <strings.h>
 #include <errno.h>
 
@@ -21,8 +22,9 @@ int netframe_to_net(int net_socket, unsigned char *netframe, int length) {
 }
 
 int main(int argc, char**argv) {
-    int sockfd, i, n=1;
+    int sockfd, i, tcp_packet_nr, n =1;
     struct sockaddr_in servaddr;
+    fd_set rset;
     unsigned char netframe[FRAME_SIZE];
     unsigned char recvline[1000];
     
@@ -52,7 +54,7 @@ int main(int argc, char**argv) {
     servaddr.sin_port=htons(15731);
 
     if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) {
-        printf("can't connect to socket - terminating\n");
+        printf("can't connect to TCP socket\n");
         exit(1);
     }
 
@@ -60,19 +62,32 @@ int main(int argc, char**argv) {
     memcpy(netframe, GETCONFIG,5);
     memcpy(&netframe[5], argv[1], strlen(argv[1]));
     if (netframe_to_net(sockfd, netframe, FRAME_SIZE)) {
-        printf("can't send data on TCP socket - terminating\n");
+        printf("can't send data on TCP socket\n");
         exit(1);
     }
 
-    while(n) {
-        if ((n=recv(sockfd,recvline,10000,0)) > 0) {
-            for ( i=0; i<n; i++) {
-                if (( i % 13 ) == 0) {
-                    printf("\n");
+    FD_ZERO(&rset);
+    FD_SET(0,&rset);
+    tcp_packet_nr=0;
+
+    for(;;) {
+        FD_SET(sockfd,&rset);
+
+        if(select(sockfd+1,&rset,NULL,NULL,NULL)<0) {
+            printf("connection lost\n");
+            exit(0);
+        }
+        tcp_packet_nr++;
+        if (FD_ISSET(sockfd,&rset)) {
+            if ((n=recv(sockfd,recvline,10000,0)) > 0) {
+                for ( i=0; i<n; i++) {
+                    if (( i % 13 ) == 0) {
+                        printf("\n %04d: ", tcp_packet_nr);
+                    }
+                    printf("%02x ", recvline[i]);
                 }
-                printf("%02x ", recvline[i]);
+                printf("\n");
             }
-            printf("\n");
         }
     }
     return 1;
