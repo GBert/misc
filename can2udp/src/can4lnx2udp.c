28e9a39a59350e8fffd07be8dede214ac96b1220
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2015-01-13 16:31:43 +0100

    add can4linux test code

---
+++ can2udp/src/can4lnx2udp.c
@@ -0,0 +1,239 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <info@gerhard-bertelsmann.de> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Gerhard Bertelsmann
+ * ----------------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <libgen.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <linux/can.h>
+#include "can4linux.h"
+
+#define MAXDG   4096		/* maximum datagram size */
+#define MAXUDP  16		/* maximum datagram size */
+
+unsigned char udpframe[MAXDG];
+unsigned char udpframe_reply[MAXDG];
+
+void print_usage(char *prg) {
+    fprintf(stderr, "\nUsage: %s -l <port> -d <port> -i <can device>\n", prg);
+    fprintf(stderr, "   Version 0.1\n\n");
+    fprintf(stderr, "         -l <port>           listening UDP port for the server - default 15730\n");
+    fprintf(stderr, "         -d <port>           destination UDP port for the server - default 15731\n");
+    fprintf(stderr, "         -b <broadcast_addr> broadcast address - default 255.255.255.255\n");
+    fprintf(stderr, "         -i <can int>        can device - default can0\n");
+    fprintf(stderr, "         -f                  running in foreground\n\n");
+}
+
+int main(int argc, char **argv) {
+    pid_t pid;
+    extern int optind, opterr, optopt;
+    int opt;
+    canmsg_t frame;
+
+    int sa,sb;			/* UDP socket UDP Broadcast Socket */
+    int fdc;			/* can4linux FD */
+    struct sockaddr_in saddr, baddr;
+
+    fd_set readfds;
+
+    int s, i;
+
+    int local_port = 15730;
+    int destination_port = 15731;
+    int verbose = 0;
+    int background = 1;
+    int canid = 0;
+    char can_interface[64];
+    const int on = 1;
+    const char broadcast_address[] = "255.255.255.255";
+
+    bzero(&saddr, sizeof(saddr));
+    bzero(&baddr, sizeof(baddr));
+    bzero(&frame, sizeof(frame));
+    bzero(udpframe, sizeof(udpframe));
+    bzero(udpframe_reply, sizeof(udpframe_reply));
+
+    /* prepare udp destination struct with defaults */
+    baddr.sin_family = AF_INET;
+    baddr.sin_port = htons(destination_port);
+    s = inet_pton(AF_INET, broadcast_address, &baddr.sin_addr);
+    if (s <= 0) {
+        if (s == 0) {
+            fprintf(stderr, "UDP IP invalid\n");
+        } else {
+            fprintf(stderr, "invalid address family\n");
+        }
+        exit(1);
+    }
+
+    while ((opt = getopt(argc, argv, "l:d:b:i:hfv?")) != -1) {
+	switch (opt) {
+	case 'l':
+	    local_port = strtoul(optarg, (char **) NULL, 10);
+	    break;
+	case 'd':
+	    destination_port = strtoul(optarg, (char **) NULL, 10);
+	    baddr.sin_port = htons(destination_port);
+	    break;
+	case 'b':
+	    s = inet_pton(AF_INET, optarg, &baddr.sin_addr);
+	    if (s <= 0) {
+		if (s == 0) {
+		    fprintf(stderr, "invalid IP address: %s\n", strerror(errno));
+		} else {
+		    fprintf(stderr, "inet_pton error: %s\n", strerror(errno));
+		}
+		exit(1);
+	    }
+	    break;
+	case 'i':
+	    strcpy(can_interface, optarg);
+	    break;
+
+	case 'v':
+	    verbose = 1;
+	    break;
+	case 'f':
+	    background = 0;
+	    break;
+
+	case 'h':
+	case '?':
+	    print_usage(basename(argv[0]));
+	    exit(0);
+
+	default:
+	    fprintf(stderr, "Unknown option %c\n", opt);
+	    print_usage(basename(argv[0]));
+	    exit(1);
+	}
+    }
+
+    if ((sa = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+	fprintf(stderr, "UDP socket error: %s\n", strerror(errno));
+	exit(1);
+    }
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(local_port);
+
+    if  (bind(sa, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+	fprintf(stderr, "UDP bind error: %s\n", strerror(errno));
+	exit(1);
+    }
+
+    if ((fdc = open(can_interface, O_RDWR)) < 0 ) {
+	fprintf(stderr, "can't open CAN interface: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    /* prepare UDP sending socket */
+    if ((sb = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+	fprintf(stderr, "Send UDP socket error %s\n", strerror(errno));
+	exit(1);
+    }
+    if (setsockopt(sb, SOL_SOCKET, SO_BROADCAST, &on, sizeof(on)) < 0) {
+	fprintf(stderr, "UDP set socket option error: %s\n", strerror(errno));
+	exit(1);
+    }
+
+    if (background) {
+	/* Fork off the parent process */
+	pid = fork();
+	if (pid < 0) {
+	    exit(EXIT_FAILURE);
+	}
+	/* If we got a good PID, then we can exit the parent process */
+
+	if (pid > 0) {
+	    printf("Going into background ...\n");
+	    exit(EXIT_SUCCESS);
+	}
+    }
+
+    while (1) {
+	FD_ZERO(&readfds);
+	FD_SET(fdc, &readfds);
+	FD_SET(sa, &readfds);
+
+	if (select((fdc > sa) ? fdc + 1 : sa + 1, &readfds, NULL, NULL, NULL) <0 ) {
+            fprintf(stderr, "select error: %s\n", strerror(errno));
+        };
+
+	/* received a CAN frame */
+	if (FD_ISSET(fdc, &readfds)) {
+
+	    if (read(fdc, &frame, 1) < 0) {
+		fprintf(stderr, "CAN read error: %s\n", strerror(errno));
+
+		/* send UDP frame */
+		if (sendto(sb, udpframe, 13, 0, (struct sockaddr *) &baddr, sizeof(baddr)) !=13)
+		    fprintf(stderr, "UDP write error: %s\n", strerror(errno));
+
+		if (verbose && !background) {
+		    printf("->CAN>UDP CANID 0x%08lX R", frame.id);
+		    printf(" [%d]", udpframe[4]);
+		    for (i = 5; i < 5 + frame.length; i++) {
+			printf(" %02x", udpframe[i]);
+		    }
+		    printf("\n");
+		}
+	    }
+	}
+	/* received a UDP packet */
+	if (FD_ISSET(sa, &readfds)) {
+	    if (read(sa, udpframe, MAXDG) == 13) {
+		memcpy(&canid, &udpframe[0], 4);
+		/* CAN is stored in network Big Endian format */
+		frame.id = ntohl(canid);
+
+		frame.id &= CAN_EFF_MASK;
+		frame.id |= CAN_EFF_FLAG;
+
+		frame.length = udpframe[4];
+
+		memcpy(&frame.data, &udpframe[5], 8);
+
+		/* send CAN frame */
+		memcpy(&frame.data, &udpframe[5], 8);
+
+		if (write(fdc, &frame, 1) != 1)
+		    fprintf(stderr, "CAN write error: %s\n", strerror(errno));
+
+		if (verbose && !background) {
+		    printf("<-UDP>CAN CANID 0x%08lX  ",
+			   frame.id & CAN_EFF_MASK);
+		    printf(" [%d]", udpframe[4]);
+		    for (i = 5; i < 5 + frame.length; i++) {
+			printf(" %02x", udpframe[i]);
+		    }
+		    printf("\n");
+
+		}
+	    }
+	}
+    }
+    close(sa);
+    close(sb);
+    return 0;
+}
