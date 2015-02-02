76eb0bc714d0d85481a707ba06ff4356a38bae33
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2015-02-02 19:01:33 +0100

    fix ubuntu init script

--- can2udp/config/ubuntu.can2lan.sh
+++ can2udp/config/ubuntu.can2lan.sh
@@ -15,29 +15,34 @@
 
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 DAEMON=/usr/bin/can2lan
-PIDFILE=/var/run/can2lan.pid
-OPTIONS="-s -c /var/www/html/cs2 -i /dev/ttyUSB0"
+OPTIONS="-c /var/www/html/config"
+NAME=can2lan
+DESC="M*rklin CAN to LAN gateway"
 test -x $DAEMON || exit 0
 
 . /lib/lsb/init-functions
 
 case "$1" in
   start)
-        log_daemon_msg "Starting CAN to LAN gateway" "can2lan"
-        start_daemon -p $PIDFILE $DAEMON $OPTIONS
-        log_end_msg $?
+        echo -n "Starting $DESC: "
+        start-stop-daemon --start --verbose --pidfile /var/run/$NAME.pid \
+                --oknodo --exec $DAEMON -- $OPTIONS
+        echo "$NAME."
+	pidof can2lan > /var/run/$NAME.pid
     ;;
   stop)
-        log_daemon_msg "Stopping CAN to LAN" "can2lan"
-        killproc -p $PIDFILE $DAEMON
-        log_end_msg $?
+        echo -n "Stopping $DESC: "
+        start-stop-daemon --stop --verbose --pidfile /var/run/$NAME.pid \
+                --oknodo --exec $DAEMON
+        echo "$NAME."
     ;;
   force-reload|restart)
     $0 stop
+    sleep 1
     $0 start
     ;;
   status)
-    status_of_proc -p $PIDFILE $DAEMON can2lan && exit 0 || exit $?
+    status_of_proc -p /var/run/$NAME.pid $DAEMON can2lan && exit 0 || exit $?
     ;;
   *)
     echo "Usage: /etc/init.d/can2lan {start|stop|restart|force-reload|status}"
