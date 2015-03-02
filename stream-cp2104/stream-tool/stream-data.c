357e04c970a4df6d8b7dcc0d92d1dfbe607cba4e
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2015-03-02 16:09:47 +0100

    only send 16 bytes and reformat code

--- stream-cp2104/stream-tool/stream-data.c
+++ stream-cp2104/stream-tool/stream-data.c
@@ -17,100 +17,99 @@
 /* CP2104 GPIO */
 #define GPIO_0_ON	0x00010001
 #define GPIO_0_OFF	0x00000001
-#define GPIO_1_ON	GPIO_0_ON << 1
+#define GPIO_1_ON	GPIO_0_ON  << 1
 #define GPIO_1_OFF	GPIO_0_OFF << 1
-#define GPIO_2_ON	GPIO_0_ON << 2
+#define GPIO_2_ON	GPIO_0_ON  << 2
 #define GPIO_2_OFF	GPIO_0_OFF << 2
-#define GPIO_3_ON	GPIO_0_ON << 3
+#define GPIO_3_ON	GPIO_0_ON  << 3
 #define GPIO_3_OFF	GPIO_0_OFF << 3
 
-
 void options(int fd, struct termios *old, struct termios *new, int speed) {
-	fcntl(fd, F_SETFL, O_NONBLOCK | fcntl(fd, F_GETFL));
+    fcntl(fd, F_SETFL, O_NONBLOCK | fcntl(fd, F_GETFL));
 
-	tcgetattr(fd, old);
-	tcgetattr(fd, new);
+    tcgetattr(fd, old);
+    tcgetattr(fd, new);
 
-        new->c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
-        new->c_oflag &= ~(OPOST);
-        new->c_cflag &= ~(CSIZE | PARENB);
-        new->c_cflag |= (CS6);
-        /* new->c_cflag |= (BOTHER); */
-        new->c_cflag &= ~CBAUD;;
-        new->c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
-        new->c_cc[VMIN] = 1;
-        new->c_cc[VTIME] = 0;
+    new->c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+    new->c_oflag &= ~(OPOST);
+    new->c_cflag &= ~(CSIZE | PARENB);
+    new->c_cflag |= (CS6);
+    /* new->c_cflag |= (BOTHER); */
+    new->c_cflag &= ~CBAUD;;
+    new->c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    new->c_cc[VMIN] = 1;
+    new->c_cc[VTIME] = 0;
 
-        cfsetispeed(new, speed);
-        cfsetospeed(new, speed);
+    cfsetispeed(new, speed);
+    cfsetospeed(new, speed);
 
-        tcflush(fd, TCIOFLUSH);
-        tcsetattr(fd, TCSANOW, new);
+    tcflush(fd, TCIOFLUSH);
+    tcsetattr(fd, TCSANOW, new);
 }
 
 int fdwrite(int fd, char *buf, int buflen) {
-	int rc, nb = 0;
+    int rc, nb = 0;
 
-	while (nb < buflen) {
-		rc = write(fd, buf, buflen);
-		if (rc < 0) {
-			if (errno == EINTR || errno == EAGAIN)
-				continue;
-			perror("write()");
-			exit(1);
-		}
-		if (rc == 0)
-			return nb;
-		nb += rc;
+    while (nb < buflen) {
+	rc = write(fd, buf, buflen);
+	if (rc < 0) {
+	    if (errno == EINTR || errno == EAGAIN)
+		continue;
+	    perror("write()");
+	    exit(1);
 	}
-	return nb;
+	if (rc == 0)
+	    return nb;
+	nb += rc;
+    }
+    return nb;
 }
 
 int main(int argc, char *argv[]) {
-	int fduart;
-	int speed = 10000;
-	uint32_t gpio;
-	struct termios oldfduart, newfduart;
-	char buffer[1024];
+    int fduart;
+    int speed = 10000;
+    uint32_t gpio;
+    struct termios oldfduart, newfduart;
+    char buffer[1024];
 
-	if (argc < 2) {
-		fprintf(stderr, "Try: %s /dev/ttyUSB0 [10000]\n", argv[0]);
-		exit(1);
-	}
+    if (argc < 2) {
+	fprintf(stderr, "Try: %s /dev/ttyUSB0 [10000]\n", argv[0]);
+	exit(1);
+    }
 
-	fduart = open(argv[1], O_RDWR | O_NOCTTY | O_NONBLOCK);
-	if (fduart < 0) {
-		perror("open()");
-		exit(1);
-	}
+    fduart = open(argv[1], O_RDWR | O_NOCTTY | O_NONBLOCK);
+    if (fduart < 0) {
+	perror("open()");
+	exit(1);
+    }
 
-	if (argc > 2) {	
-		speed = strtoul(argv[1], (char **)NULL, 10);
-	}
+    if (argc > 2) {
+	speed = strtoul(argv[1], (char **)NULL, 10);
+    }
 
-	options(fduart, &oldfduart, &newfduart, speed);
+    options(fduart, &oldfduart, &newfduart, speed);
 
-	/* reset GPIOs */
-	gpio = 0x0000000F;
-	ioctl(fduart, IOCTL_GPIOSET, &gpio);
+    /* reset GPIOs */
+    gpio = 0x0000000F;
+    ioctl(fduart, IOCTL_GPIOSET, &gpio);
 
-	gpio = GPIO_0_OFF;
-	ioctl(fduart, IOCTL_GPIOSET, &gpio);
+    gpio = GPIO_0_OFF;
+    ioctl(fduart, IOCTL_GPIOSET, &gpio);
 
-	memset(buffer, 0x15, 16);
+    memset(buffer, 0x15, 16);
 /*	for (int i = 0; i<= 10; i++) */
-	    fdwrite(fduart, buffer, 64);
+    fdwrite(fduart, buffer, 16);
 
-	/* tcflush(fduart, TCIFLUSH);
-	tcsetattr(fduart, TCSANOW, &oldfduart); */
-	gpio = GPIO_0_ON;
-	ioctl(fduart, IOCTL_GPIOSET, &gpio);
+    gpio = GPIO_0_ON;
+    ioctl(fduart, IOCTL_GPIOSET, &gpio);
 
-	memset(buffer, 0x46, 16);
+    memset(buffer, 0x46, 16);
 /*	for (int i = 0; i<= 10; i++) */
-	    fdwrite(fduart, buffer, 64);
+    fdwrite(fduart, buffer, 16);
 
-	close(fduart);
+    /* tcflush(fduart, TCIFLUSH);
+    tcsetattr(fduart, TCSANOW, &oldfduart); */
+    close(fduart);
 
-	return 0;
+    return 0;
 }
