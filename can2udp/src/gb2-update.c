8816f4c2779b92f1b8049e9412747a982cb6e149
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2015-03-24 20:29:08 +0100

    put back padding

--- can2udp/src/gb2-update.c
+++ can2udp/src/gb2-update.c
@@ -36,10 +36,12 @@
 #define GB2_BLOCK_SIZE		512
 #define GB2_BLOCK_SHIFT		9	/* 2^9 = 512 */
 #define GB2_BOOT_BLOCK_SIZE	2
+#define GB2_FILL_SBLOCK		8-1
 
 #define MS2_BLOCK_SIZE		1024
 #define MS2_BLOCK_SHIFT		10	/* 2^10 = 1024 */
 #define MS2_BOOT_BLOCK_SIZE	0
+#define GB2_FILL_SBLOCK		1024-1
 
 #define FILL_SBLOCK		8-1
 
@@ -47,6 +49,7 @@ struct update_config {
     int block_size;
     int shift;
     int boot_blocks;
+    int padding;
 };
 
 extern uint16_t CRCCCITT(uint8_t * data, size_t length, uint16_t seed);
@@ -261,7 +264,7 @@ void fsm(unsigned char *netframe) {
     case (0x00310000UL):
 	printf("received CAN Ping answer\n");
 	/* print_can_frame(" ", netframe, 1); */
-	/* if ((netframe[5] == 0x47 ) && (netframe[6] == 0x42 )) { */
+	/* if ((netframe[5] == 0x47 ) && (netframe[6] == 0x43 )) { */
 	if ((netframe[4] == 8) && (netframe[5] == 0x47)) {
 	    memcpy(&gb2_id, &netframe[5], 4);
 	    memcpy(&version, &netframe[9], 2);
