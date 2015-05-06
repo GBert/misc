b950bd0f90114f5a07d567daa2466b204050d228
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2015-05-06 18:41:43 +0200

    prepare S88 kthread task

--- s88-gpio/src/s88-gpio.c
+++ s88-gpio/src/s88-gpio.c
@@ -10,6 +10,9 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/gpio.h>
+#include <linux/kobject.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
 
 #define DRV_NAME "S88_GPIO"
 
@@ -24,6 +27,18 @@ static int gpios [] = {20, 19, 18, 7, 255};
 static int gpio_count;
 module_param_array(gpios, int, &gpio_count, 0);
 MODULE_PARM_DESC(gpios, "used GPIOs for Load, Reset, Clock, Data-In, Data-Out");
+
+static struct kobject *s88_kobj;
+static struct task_struct *task;
+
+static int s88_task(void *arg) {
+    while(!kthread_should_stop()) {
+	set_current_state(TASK_RUNNING);
+	set_current_state(TASK_INTERRUPTIBLE);
+	msleep(2);
+    }
+   return 0;
+}
 
 static int __init mymodule_init(void) {
     if (gpio_request(gpios[GPIO_LOAD], "S88 Load")) {
@@ -62,7 +77,7 @@ static int __init mymodule_init(void) {
 	goto FREE_DATA_IN;
     }
 
-
+    /* normally S88 is data in only */
     if (gpios[GPIO_DATA_OUT] != 255) {
 	if (gpio_request(gpios[GPIO_DATA_OUT], "S88 Data Out")) {
 	    printk (KERN_ERR "can't get GPIO %d as S88 Data Out\n", gpios[GPIO_DATA_OUT]);
@@ -73,6 +88,19 @@ static int __init mymodule_init(void) {
 	    goto FREE_DATA_OUT;
 	}
     }
+
+    s88_kobj = kobject_create_and_add("s88", kernel_kobj->parent); // kernel_kobj points to /sys/kernel
+    if(!s88_kobj){
+	printk(KERN_ALERT "S88: failed to create kobject mapping\n");
+	return -ENOMEM;
+    }
+
+    task = kthread_run(s88_task, NULL, "s88_thread");
+    if(IS_ERR(task)) {
+	printk(KERN_ALERT "S88: failed to create the reading task\n");
+	goto FREE_DATA_OUT;
+    }
+
     return(0);
 
 FREE_DATA_OUT:
@@ -89,6 +117,8 @@ FREE_LOAD:
 }
 
 static void __exit mymodule_exit(void) {
+   kthread_stop(task);
+   kobject_put(s88_kobj);
    printk (KERN_INFO "freeing GPIOs \n");
    gpio_free(gpios[GPIO_LOAD]);
    gpio_free(gpios[GPIO_RESET]);
