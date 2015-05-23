b44939a250f0e7ecc935f4d66e129544cd4112a8
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2015-05-23 13:24:36 +0200

    na

--- nec-decode/pic12f-test.c
+++ nec-decode/pic12f-test.c
@@ -1,10 +1,10 @@
 /*
-* ----------------------------------------------------------------------------
+* -----------------------------------------------------------------------------
 * "THE BEER-WARE LICENSE" (Revision 42):
-* Sergeant Hartman & friends wrote this file. As long as you retain this notice you
+* Sgt. Hartman & friends wrote this file. As long as you retain this notice you
 * can do whatever you want with this stuff. If we meet some day, and you think
-* this stuff is worth it, you can buy me a beer in return
-* ----------------------------------------------------------------------------
+* this stuff is worth it, you can buy us a beer in return
+* -----------------------------------------------------------------------------
 */
 
 /* inspired by Linux/drivers/media/rc/ir-nec-decoder.c */
