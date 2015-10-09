53416367f0881cb02fb20cf3e4f2ee06efadc320
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2015-10-09 10:42:05 +0200

    lindent some more files

--- can2udp/src/slre/unit_test.c
+++ can2udp/src/slre/unit_test.c
@@ -18,8 +18,8 @@ static int static_failed_tests = 0;
 } while (0)
 
 /* Regex must have exactly one bracket pair */
-static char *slre_replace(const char *regex, const char *buf,
-                          const char *sub) {
+static char *slre_replace(const char *regex, const char *buf, const char *sub)
+{
   char *s = NULL;
   int n, n1, n2, n3, s_len, len = strlen(buf);
   struct slre_cap cap = { NULL, 0 };
@@ -27,12 +27,11 @@ static char *slre_replace(const char *regex, const char *buf,
   do {
     s_len = s == NULL ? 0 : strlen(s);
     if ((n = slre_match(regex, buf, len, &cap, 1, 0)) > 0) {
-      n1 = cap.ptr - buf, n2 = strlen(sub),
-         n3 = &buf[n] - &cap.ptr[cap.len];
+      n1 = cap.ptr - buf, n2 = strlen(sub), n3 = &buf[n] - &cap.ptr[cap.len];
     } else {
       n1 = len, n2 = 0, n3 = 0;
     }
-    s = (char *) realloc(s, s_len + n1 + n2 + n3 + 1);
+    s = (char *)realloc(s, s_len + n1 + n2 + n3 + 1);
     memcpy(s + s_len, buf, n1);
     memcpy(s + s_len + n1, sub, n2);
     memcpy(s + s_len + n1 + n2, cap.ptr + cap.len, n3);
@@ -55,8 +54,7 @@ int main(void) {
   ASSERT(slre_match("x|$", "abcd", 4, NULL, 0, 0) == 4);
   ASSERT(slre_match("x", "abcd", 4, NULL, 0, 0) == SLRE_NO_MATCH);
   ASSERT(slre_match(".", "abcd", 4, NULL, 0, 0) == 1);
-  ASSERT(slre_match("^.*\\\\.*$", "c:\\Tools", 8, NULL, 0, SLRE_IGNORE_CASE)
-    == 8);
+  ASSERT(slre_match("^.*\\\\.*$", "c:\\Tools", 8, NULL, 0, SLRE_IGNORE_CASE) == 8);
   ASSERT(slre_match("\\", "a", 1, NULL, 0, 0) == SLRE_INVALID_METACHARACTER);
   ASSERT(slre_match("\\x", "a", 1, NULL, 0, 0) == SLRE_INVALID_METACHARACTER);
   ASSERT(slre_match("\\x1", "a", 1, NULL, 0, 0) == SLRE_INVALID_METACHARACTER);
@@ -64,17 +62,14 @@ int main(void) {
 
   ASSERT(slre_match("^.+$", "", 0, NULL, 0, 0) == SLRE_NO_MATCH);
   ASSERT(slre_match("^(.+)$", "", 0, NULL, 0, 0) == SLRE_NO_MATCH);
-  ASSERT(slre_match("^([\\+-]?)([\\d]+)$", "+", 1,
-                    caps, 10, SLRE_IGNORE_CASE) == SLRE_NO_MATCH);
-  ASSERT(slre_match("^([\\+-]?)([\\d]+)$", "+27", 3,
-                    caps, 10, SLRE_IGNORE_CASE) == 3);
+  ASSERT(slre_match("^([\\+-]?)([\\d]+)$", "+", 1, caps, 10, SLRE_IGNORE_CASE) == SLRE_NO_MATCH);
+  ASSERT(slre_match("^([\\+-]?)([\\d]+)$", "+27", 3, caps, 10, SLRE_IGNORE_CASE) == 3);
   ASSERT(caps[0].len == 1);
   ASSERT(caps[0].ptr[0] == '+');
   ASSERT(caps[1].len == 2);
   ASSERT(memcmp(caps[1].ptr, "27", 2) == 0);
 
-  ASSERT(slre_match("tel:\\+(\\d+[\\d-]+\\d)",
-                    "tel:+1-201-555-0123;a=b", 23, caps, 10, 0) == 19);
+  ASSERT(slre_match("tel:\\+(\\d+[\\d-]+\\d)", "tel:+1-201-555-0123;a=b", 23, caps, 10, 0) == 19);
   ASSERT(caps[0].len == 14);
   ASSERT(memcmp(caps[0].ptr, "1-201-555-0123", 14) == 0);
 
@@ -100,10 +95,8 @@ int main(void) {
   /* Flags - case sensitivity */
   ASSERT(slre_match("FO", "foo", 3, NULL, 0, 0) == SLRE_NO_MATCH);
   ASSERT(slre_match("FO", "foo", 3, NULL, 0, SLRE_IGNORE_CASE) == 2);
-  ASSERT(slre_match("(?m)FO", "foo", 3, NULL, 0, 0) ==
-    SLRE_UNEXPECTED_QUANTIFIER);
-  ASSERT(slre_match("(?m)x", "foo", 3, NULL, 0, 0) ==
-    SLRE_UNEXPECTED_QUANTIFIER);
+  ASSERT(slre_match("(?m)FO", "foo", 3, NULL, 0, 0) == SLRE_UNEXPECTED_QUANTIFIER);
+  ASSERT(slre_match("(?m)x", "foo", 3, NULL, 0, 0) == SLRE_UNEXPECTED_QUANTIFIER);
 
   ASSERT(slre_match("fo", "foo", 3, NULL, 0, 0) == 2);
   ASSERT(slre_match(".+", "foo", 3, NULL, 0, 0) == 3);
@@ -164,16 +157,13 @@ int main(void) {
   ASSERT(slre_match("ab(cd)+?ef", "abcdcdef", 8, NULL, 0, 0) == 8);
   ASSERT(slre_match("ab(cd)+?.", "abcdcdef", 8, NULL, 0, 0) == 5);
   ASSERT(slre_match("ab(cd)?", "abcdcdef", 8, NULL, 0, 0) == 4);
-  ASSERT(slre_match("a(b)(cd)", "abcdcdef", 8, caps, 1, 0) ==
-      SLRE_CAPS_ARRAY_TOO_SMALL);
-  ASSERT(slre_match("(.+/\\d+\\.\\d+)\\.jpg$", "/foo/bar/12.34.jpg", 18,
-                    caps, 1, 0) == 18);
+  ASSERT(slre_match("a(b)(cd)", "abcdcdef", 8, caps, 1, 0) == SLRE_CAPS_ARRAY_TOO_SMALL);
+  ASSERT(slre_match("(.+/\\d+\\.\\d+)\\.jpg$", "/foo/bar/12.34.jpg", 18, caps, 1, 0) == 18);
   ASSERT(slre_match("(ab|cd).*\\.(xx|yy)", "ab.yy", 5, NULL, 0, 0) == 5);
   ASSERT(slre_match(".*a", "abcdef", 6, NULL, 0, 0) == 1);
   ASSERT(slre_match("(.+)c", "abcdef", 6, NULL, 0, 0) == 3);
   ASSERT(slre_match("\\n", "abc\ndef", 7, NULL, 0, 0) == 4);
-  ASSERT(slre_match("b.\\s*\\n", "aa\r\nbb\r\ncc\r\n\r\n", 14,
-                    caps, 10, 0) == 8);
+  ASSERT(slre_match("b.\\s*\\n", "aa\r\nbb\r\ncc\r\n\r\n", 14, caps, 10, 0) == 8);
 
   /* Greedy vs non-greedy */
   ASSERT(slre_match(".+c", "abcabc", 6, NULL, 0, 0) == 6);
@@ -210,19 +200,16 @@ int main(void) {
   ASSERT(slre_match(".*$", "ab", 2, NULL, 0, 0) == 2);
   ASSERT(slre_match("a+$", "aa", 2, NULL, 0, 0) == 2);
   ASSERT(slre_match("a*$", "aa", 2, NULL, 0, 0) == 2);
-  ASSERT(slre_match( "a+$" ,"Xaa", 3, NULL, 0, 0) == 3);
-  ASSERT(slre_match( "a*$" ,"Xaa", 3, NULL, 0, 0) == 3);
+  ASSERT(slre_match("a+$", "Xaa", 3, NULL, 0, 0) == 3);
+  ASSERT(slre_match("a*$", "Xaa", 3, NULL, 0, 0) == 3);
 
   {
     /* Example: HTTP request */
     const char *request = " GET /index.html HTTP/1.0\r\n\r\n";
     struct slre_cap caps[4];
 
-    if (slre_match("^\\s*(\\S+)\\s+(\\S+)\\s+HTTP/(\\d)\\.(\\d)",
-                   request, strlen(request), caps, 4, 0) > 0) {
-      printf("Method: [%.*s], URI: [%.*s]\n",
-             caps[0].len, caps[0].ptr,
-             caps[1].len, caps[1].ptr);
+    if (slre_match("^\\s*(\\S+)\\s+(\\S+)\\s+HTTP/(\\d)\\.(\\d)", request, strlen(request), caps, 4, 0) > 0) {
+      printf("Method: [%.*s], URI: [%.*s]\n", caps[0].len, caps[0].ptr, caps[1].len, caps[1].ptr);
     } else {
       printf("Error parsing [%s]\n", request);
     }
@@ -234,8 +221,8 @@ int main(void) {
   {
     /* Example: string replacement */
     char *s = slre_replace("({{.+?}})",
-                           "Good morning, {{foo}}. How are you, {{bar}}?",
-                           "Bob");
+			   "Good morning, {{foo}}. How are you, {{bar}}?",
+			   "Bob");
     printf("%s\n", s);
     ASSERT(strcmp(s, "Good morning, Bob. How are you, Bob?") == 0);
     free(s);
@@ -244,23 +231,20 @@ int main(void) {
   {
     /* Example: find all URLs in a string */
     static const char *str =
-      "<img src=\"HTTPS://FOO.COM/x?b#c=tab1\"/> "
-      "  <a href=\"http://cesanta.com\">some link</a>";
+	"<img src=\"HTTPS://FOO.COM/x?b#c=tab1\"/>   <a href=\"http://cesanta.com\">some link</a>";
 
     static const char *regex = "(?i)((https?://)[^\\s/'\"<>]+/?[^\\s'\"<>]*)";
     struct slre_cap caps[2];
-    int i, j = 0, str_len = (int) strlen(str);
+    int i, j = 0, str_len = (int)strlen(str);
 
-    while (j < str_len &&
-           (i = slre_match(regex, str + j, str_len - j, caps, 2, 0)) > 0) {
+    while (j < str_len && (i = slre_match(regex, str + j, str_len - j, caps, 2, 0)) > 0) {
       printf("Found URL: [%.*s]\n", caps[0].len, caps[0].ptr);
       j += i;
     }
   }
 
   printf("Unit test %s (total test: %d, failed tests: %d)\n",
-         static_failed_tests > 0 ? "FAILED" : "PASSED",
-         static_total_tests, static_failed_tests);
+	 static_failed_tests > 0 ? "FAILED" : "PASSED", static_total_tests, static_failed_tests);
 
   return static_failed_tests == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
