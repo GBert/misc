97d3b56ca97276da06a47bdf8f035353b7846db7
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2015-10-29 18:44:59 +0100

    code cleaning

--- nec-decode/nec-test.c
+++ nec-decode/nec-test.c
@@ -15,11 +15,11 @@
 #include <stdint.h>
 
 enum nec_state { STATE_INACTIVE,
-	STATE_HEADER_SPACE,
-	STATE_BIT_PULSE,
-	STATE_BIT_SPACE,
-	STATE_TRAILER_PULSE,
-	STATE_TRAILER_SPACE
+    STATE_HEADER_SPACE,
+    STATE_BIT_PULSE,
+    STATE_BIT_SPACE,
+    STATE_TRAILER_PULSE,
+    STATE_TRAILER_SPACE
 };
 
 #define TIMER_GRID		64000	/* 64us */
@@ -29,31 +29,30 @@ enum nec_state { STATE_INACTIVE,
 #define	T_HIGH(x)	(( x * NEC_UNIT / TIMER_GRID) * (100 + MARGIN)/100)
 
 #define NEC_NBITS               32
-#define NEC_UNIT                562500  /* ns */
+#define NEC_UNIT                562500	/* ns */
 #define NEC_HEADER_PULSE        (16 * NEC_UNIT)
-#define NECX_HEADER_PULSE       (8  * NEC_UNIT) /* Less common NEC variant */
+#define NECX_HEADER_PULSE       (8  * NEC_UNIT)	/* Less common NEC variant */
 #define NEC_HEADER_SPACE        (8  * NEC_UNIT)
 #define NEC_REPEAT_SPACE        (4  * NEC_UNIT)
 #define NEC_BIT_PULSE           (1  * NEC_UNIT)
 #define NEC_BIT_0_SPACE         (1  * NEC_UNIT)
 #define NEC_BIT_1_SPACE         (3  * NEC_UNIT)
 #define NEC_TRAILER_PULSE       (1  * NEC_UNIT)
-#define NEC_TRAILER_SPACE       (10 * NEC_UNIT) /* even longer in reality */
+#define NEC_TRAILER_SPACE       (10 * NEC_UNIT)	/* even longer in reality */
 #define NECX_REPEAT_BITS        1
 
-
-
 uint8_t test_data []   = { 140, 70, 8,  8,  8,  8,  8, 26,  8, 26,   8,  8,  8, 26,  8,  8,  8, 26,
-                                    8,  8,  8, 26,  8,  8,  8,  8,   8, 26,  8,  8,  8, 26,  8,  8,
-                                    8, 26,  8, 26,  8, 26,  8, 26,   8,  8,  8,  8,  8,  8,  8,  8,
-                                    8,  8,  8,  8,  8,  8,  8,  8,   8, 26,  8, 26,  8, 26,  8, 26,  8, 255 };
+				    8,  8,  8, 26,  8,  8,  8,  8,   8, 26,  8,  8,  8, 26,  8,  8,
+				    8, 26,  8, 26,  8, 26,  8, 26,   8,  8,  8,  8,  8,  8,  8,  8,
+				    8,  8,  8,  8,  8,  8,  8,  8,   8, 26,  8, 26,  8, 26,  8, 26,  8, 255 };
 
 uint8_t test_data_r [] = { 140, 45, 11, 255};
 
 uint8_t test_data_f [] = { 140, 70, 8,  8,  8,  8,  8, 26,  8, 26,   8,  8,  8, 26,  8,  8,  8, 26,
-                                    8,  8,  8, 26,  8,  8,  8,  8,   8, 26,  8,  8,  8, 26,  8,  8,
-                                    8, 26, 99, 26,  8, 26,  8, 26,   8,  8,  8,  8,  8,  8,  8,  8,
-                                    8,  8,  8,  8,  8,  8,  8,  8,   8, 26,  8, 26,  8, 26,  8, 26, 8, 255 };
+				    8,  8,  8, 26,  8,  8,  8,  8,   8, 26,  8,  8,  8, 26,  8,  8,
+				    8, 26, 99, 26,  8, 26,  8, 26,   8,  8,  8,  8,  8,  8,  8,  8,
+				    8,  8,  8,  8,  8,  8,  8,  8,   8, 26,  8, 26,  8, 26,  8, 26, 8, 255 };
+
 /* timer 50us */
 
 /*                                              /  64us grid
@@ -88,12 +87,12 @@ void ir_nec_decode(uint8_t stopwatch) {
     switch (ir_nec_decode_state) {
 
     case STATE_INACTIVE:
-	if ((stopwatch > 125 ) && (stopwatch < 156))
+	if ((stopwatch > 125) && (stopwatch < 156))
 	    ir_nec_decode_state = STATE_HEADER_SPACE;
-        break;
+	break;
 
     case STATE_HEADER_SPACE:
-	if ((stopwatch > 62 ) && (stopwatch < 78)) {
+	if ((stopwatch > 62) && (stopwatch < 78)) {
 	    /* we got the start sequence -> old data is now invalid */
 	    ir_nec_data_valid = 0;
 	    ir_nec_decode_bits = 0;
@@ -101,24 +100,24 @@ void ir_nec_decode(uint8_t stopwatch) {
 	    break;
 	}
 	/* is this a repeat sequence ? */
-	if ((stopwatch > 30 ) && (stopwatch < 41)) {
+	if ((stopwatch > 30) && (stopwatch < 41)) {
 	    /* if ir_nec_decode_bits == 32 the repeat sequence could be valid */
 	    ir_nec_decode_state = STATE_TRAILER_PULSE;
-            break;
+	    break;
 	}
 
     case STATE_BIT_PULSE:
-	if ((stopwatch > 6 ) && (stopwatch < 11)) {
+	if ((stopwatch > 6) && (stopwatch < 11)) {
 	    ir_nec_decode_state = STATE_BIT_SPACE;
 	    break;
 	}
-    
+
     case STATE_BIT_SPACE:
-	nec_code >>=1;
-	if ((stopwatch > 22 ) && (stopwatch < 30))
+	nec_code >>= 1;
+	if ((stopwatch > 22) && (stopwatch < 30))
 	    nec_code |= 0x80000000;
-        /* valid data anyway ? */
-	else if ((stopwatch < 7 ) || (stopwatch > 10)) {
+	/* valid data anyway ? */
+	else if ((stopwatch < 7) || (stopwatch > 10)) {
 	    /* TODO */
 	    ir_nec_decode_state = STATE_INACTIVE;
 	    break;
@@ -133,7 +132,7 @@ void ir_nec_decode(uint8_t stopwatch) {
 	break;
 
     case STATE_TRAILER_PULSE:
-	if ((stopwatch > 6 ) && (stopwatch < 11)) {
+	if ((stopwatch > 6) && (stopwatch < 11)) {
 	    ir_nec_decode_state = STATE_TRAILER_SPACE;
 	    break;
 	}
@@ -150,20 +149,19 @@ void ir_nec_decode(uint8_t stopwatch) {
     default:
 	ir_nec_decode_state = STATE_INACTIVE;
     }
-	
 }
 
-void feed_state_machine(uint8_t *data, int length) {
+void feed_state_machine(uint8_t * data, int length) {
     int i;
     uint8_t old_state;
     for (i = 0; i < length; i++) {
 	old_state = ir_nec_decode_state;
 	ir_nec_decode(data[i]);
 	printf("stopwatch %03d state %d -> %d  bit %02d\n", data[i], old_state, ir_nec_decode_state, ir_nec_decode_bits);
-   }
-   if (ir_nec_data_valid)
+    }
+    if (ir_nec_data_valid)
 	printf("state machine : valid data 0x%08X\n\n", nec_code);
-   else
+    else
 	printf("state machine : no valid data\n\n");
 }
 
