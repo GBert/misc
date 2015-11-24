06165480f5c58ddb1e458d5738bead7423101bd3
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2015-11-24 14:28:41 +0100

    fix S88 data setup

--- ftdi2s88/README.md
+++ ftdi2s88/README.md
@@ -2,15 +2,16 @@ S88 Interface using FTDI in SYNC-BITBANG mode
 =============================================
 
 Why ?:
+
 It's easy to use GPIOs connecting S88-Bus (http://s88udp.de/).
 But some boards don't have accessible GPIOs.
 
-With FTDI's synch bitbanging USB to serial converter you can
-build a lightweight S88 interface. It's only copying buffers
+With FTDI's synch bitbanging capable USB to serial converters you
+can build lightweight S88 interfaces. It's only copying buffers
 so it dosn't need much CPU cycles.
 
-The programm sends M\*rklin S88 events on UDP port 15730.
+The programm sends M\*rklin S88 events on UDP port 15731 (broadcast).
 The datagrams are encapsulated CAN-Frames.
 
 Plan:
-- debouncing events
+- debouncing
--- ftdi2s88/src/ftdi2s88.c
+++ ftdi2s88/src/ftdi2s88.c
@@ -16,19 +16,19 @@
 #include <arpa/inet.h>
 #include <ftdi.h>
 
-#define BAUDRATE	(4096l)
+#define BAUDRATE	(4096)
 #define S88_DEF_BITS	16
 #define FIFO_SIZE	256
 #define UDP_PORT	15731
 
-static struct ftdi_context *ftdic;
+#define BIT(x)		(1<<x)
+#define S88_LOAD	BIT(0) | BIT(4)
+#define S88_PS		BIT(1) | BIT(5)
+#define S88_CLOCK	BIT(2) | BIT(6)
+#define S88_DATA_I	BIT(3)
+#define S88_DATA_II	BIT(7)
 
-void print_usage(char *prg) {
-    fprintf(stderr, "\nUsage: %s -b baud\n", prg);
-    fprintf(stderr, "   Version 0.1\n\n");
-    fprintf(stderr, "         -b baud       baudrate - default 4096 (~50us)\n");
-    fprintf(stderr, "         -l databits   databits(e.g. 6088 -> 16)\n\n");
-}
+static struct ftdi_context *ftdic;
 
 /*
 	TXD - 0x01 -> S88 reset
@@ -44,7 +44,17 @@ void print_usage(char *prg) {
 /* reset bit 1 */
 /* ps    bit 2 */
 /* clock bit 3 */
-const uint8_t S88_INIT[] = { 0, 0, 1, 1, 5, 5, 1, 1, 3, 3, 1, 1, 0, 0 };
+const uint8_t S88_INIT[] = { 0, 0, S88_LOAD, S88_LOAD, S88_LOAD | S88_CLOCK, S88_LOAD | S88_CLOCK, S88_LOAD, S88_LOAD,
+			     S88_LOAD | S88_PS, S88_LOAD | S88_PS, S88_LOAD, S88_LOAD, 0, 0 };
+
+void print_usage(char *prg) {
+    fprintf(stderr, "\nUsage: %s -b baud\n", prg);
+    fprintf(stderr, "   Version 0.1\n\n");
+    fprintf(stderr, "         -b <bcast_addr/int> broadcast address or interface - default 255.255.255.255/br-lan\n");
+    fprintf(stderr, "         -d                  going into background\n");
+    fprintf(stderr, "         -r <baudrate>       baudrate - default 4096 (~50us)\n");
+    fprintf(stderr, "         -l <databits>       databits(e.g. 6088 -> 16)\n\n");
+}
 
 int do_init(int baudrate) {
     ftdic = ftdi_new();
@@ -54,7 +64,7 @@ int do_init(int baudrate) {
 	return -1;
     }
 
-    if (ftdi_usb_open_desc(ftdic, 0x0403, 0x6001, NULL, NULL) < 0) { 
+    if (ftdi_usb_open_desc(ftdic, 0x0403, 0x6001, NULL, NULL) < 0) {
     /* if (ftdi_usb_open_desc(ftdic, 0x0403, 0x6015, NULL, NULL) < 0) { */
 	fprintf(stderr, "ftdi_usb_open_desc failed: %s\n", ftdi_get_error_string(ftdic));
 	return -1;
@@ -70,13 +80,26 @@ int do_init(int baudrate) {
 	fprintf(stderr, "ftdi_set_baudrate failed: %s\n", ftdi_get_error_string(ftdic));
 	return -1;
     }
+
+    if (ftdi_usb_purge_tx_buffer(ftdic)) {
+	fprintf(stderr, "tx buffer flushing failed: %s\n", ftdi_get_error_string(ftdic));
+	return -1;
+    }
+
+    if (ftdi_usb_purge_rx_buffer(ftdic)) {
+	fprintf(stderr, "rx buffer flushing failed: %s\n", ftdi_get_error_string(ftdic));
+	return -1;
+    }
+
     return 0;
 }
 
 int fill_data(uint8_t * b, size_t s, int s88_bits) {
     int i, offset;
 
+    i = 0;
     offset = sizeof(S88_INIT);
+
     if (s < s88_bits * 4 + offset) {
 	fprintf(stderr, "to less space (%d) for %d bits\n", (int)s, s88_bits);
 	return -1;
@@ -86,8 +109,8 @@ int fill_data(uint8_t * b, size_t s, int s88_bits) {
     while (i < s88_bits * 4) {
 	b[i++ + offset] = 0;
 	b[i++ + offset] = 0;
-	b[i++ + offset] = 4;
-	b[i++ + offset] = 4;
+	b[i++ + offset] = S88_CLOCK;
+	b[i++ + offset] = S88_CLOCK;
     }
     return (i + offset);
 }
@@ -123,12 +146,20 @@ int main(int argc, char **argv) {
     struct timeval tm1, tm2;
     unsigned long elapsed_time;
     unsigned int baudrate;
-    int buffersize, opt, length, background, s, sb, destination_port;
+    int buffersize, opt, length, background, s, sb, destination_port, ret;
     struct sockaddr_in baddr;
     const int on = 1;
 
     char *udp_dst_address = (char *)malloc(16);
+    if (udp_dst_address == NULL) {
+	fprintf(stderr, "can't alloc udp_dst_address memory\n");
+	exit(1);
+    }
     char *bcast_interface = (char *)malloc(16);
+    if (bcast_interface == NULL) {
+	fprintf(stderr, "can't alloc bcast_interface memory\n");
+	exit(1);
+    }
 
     strcpy(udp_dst_address, "255.255.255.255");
     strcpy(bcast_interface, "br-lan");
@@ -138,9 +169,26 @@ int main(int argc, char **argv) {
     length = S88_DEF_BITS;
     background = 0;
 
-    while ((opt = getopt(argc, argv, "b:l:h?")) != -1) {
+    while ((opt = getopt(argc, argv, "b:dr:l:h?")) != -1) {
 	switch (opt) {
 	case 'b':
+	    if (strlen(optarg) <= 15) {
+		/* IP address begins with a number */
+		if ((optarg[0] >= '0') || (optarg[0] <= '9')) {
+		    strcpy(udp_dst_address, optarg);
+		} else {
+		    bzero(bcast_interface, 16);
+		    strcpy(bcast_interface, optarg);
+		}
+	    } else {
+		fprintf(stderr, "UDP broadcast address or interface error: %s\n", optarg);
+		exit(1);
+	    }
+	    break;
+	case 'd':
+	    background = 1;
+	    break;
+	case 'r':
 	    baudrate = atoi(optarg);
 	    break;
 	case 'l':
@@ -180,7 +228,8 @@ int main(int argc, char **argv) {
 	fprintf(stderr, "error creating UDP sending socket: %s\n", strerror(errno));
 	exit(1);
     }
-    if (setsockopt(sb, SOL_SOCKET, SO_BROADCAST, &on, sizeof(on)) < 0) {
+    ret = setsockopt(sb, SOL_SOCKET, SO_BROADCAST, &on, sizeof(on));
+    if (ret < 0) {
 	fprintf(stderr, "error setup UDP broadcast option: %s\n", strerror(errno));
 	exit(1);
     }
@@ -203,24 +252,36 @@ int main(int argc, char **argv) {
     if (do_init(baudrate))
 	exit(-1);
 
+    bzero(w_data, sizeof(w_data));
+
+    ret = fill_data(w_data, sizeof(w_data), length);
+
+    if (ret < 0) {
+	fprintf(stderr, "to many data bits\n");
+	exit(1);
+    }
+
 #if 1
+/* testing: simple bit pattern */
     buffersize = sizeof(w_data);
     for (int i = 0; i < buffersize; i++) {
 	w_data[i] = (uint8_t) i & 0xff;
-    }
-#elif 0
-    buffersize = fill_s88_data(length)
-	if (ret < 0) {
-	fprintf(stderr, "to many data bits\n");
-	exit(1);
     }
 #endif
 
     for (;;) {
 	gettimeofday(&tm1, NULL);
 
-	ftdi_write_data(ftdic, w_data, buffersize);
-	ftdi_read_data(ftdic, r_data, buffersize);
+	ret = ftdi_write_data(ftdic, w_data, buffersize);
+	if (ret < 0) {
+	    fprintf(stderr, "ftdi_write_data faild: %s", ftdi_get_error_string(ftdic));
+	    exit(1);
+	}
+	ret = ftdi_read_data(ftdic, r_data, buffersize);
+	if (ret < 0) {
+	    fprintf(stderr, "ftdi_read_data faild: %s", ftdi_get_error_string(ftdic));
+	    exit(1);
+	}
 
 	gettimeofday(&tm2, NULL);
 	elapsed_time = 1E6 * (tm2.tv_sec - tm1.tv_sec) + (tm2.tv_usec - tm1.tv_usec);
