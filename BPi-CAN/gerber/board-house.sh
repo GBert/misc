fcd52a8a0925d70757a694f8860b747b9b275e81
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2016-05-16 15:42:48 +0200

    fix mixed files

--- BPi-CAN/gerber/board-house.sh
+++ BPi-CAN/gerber/board-house.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-cp BPi-CAN-F.Cu.gbr      BPi_CAN.GTL
-cp BPi-CAN-B.Cu.gbr      BPi_CAN.GBL
-cp BPi-CAN-F.Mask.gbr    BPi_CAN.GTS
-cp BPi-CAN-B.Mask.gbr    BPi_CAN.GBS
-cp BPi-CAN-F.SilkS.gbr   BPi_CAN.GTO
-cp BPi-CAN-B.SilkS.gbr   BPi_CAN.GBO
+cp BPi-CAN-F_Cu.gtl      BPi_CAN.GTL
+cp BPi-CAN-B_Cu.gbl      BPi_CAN.GBL
+cp BPi-CAN-F_Mask.gts    BPi_CAN.GTS
+cp BPi-CAN-B_Mask.gbs    BPi_CAN.GBS
+cp BPi-CAN-F_SilkS.gto   BPi_CAN.GTO
+cp BPi-CAN-B_SilkS.gbo   BPi_CAN.GBO
 cp BPi-CAN.drl           BPi_CAN-PTH.TXT
 cp BPi-CAN-NPTH.drl      BPi_CAN-NPTH.TXT
-cp BPi-CAN-Edge.Cuts.gbr BPi_CAN.GML
+cp BPi-CAN-Edge_Cuts.gbr BPi_CAN.GML
 
 rm -f BPi_CAN.ZIP
 zip BPi_CAN.ZIP BPi_CAN.GTL BPi_CAN.GBL BPi_CAN.GTS BPi_CAN.GBS BPi_CAN.GTO BPi_CAN.GBO BPi_CAN-PTH.TXT BPi_CAN-NPTH.TXT BPi_CAN.GML 
