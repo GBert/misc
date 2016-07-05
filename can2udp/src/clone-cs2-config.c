010d2fdfcca4c9bde40a090c1e2914582219d6cf
eh2be5j <info@gerhard-bertelsmann.de>
2016-07-05 15:27:36 +0200

    first working CS2.exe complete cloning

--- can2udp/src/clone-cs2-config.c
+++ can2udp/src/clone-cs2-config.c
@@ -24,9 +24,22 @@
 #define FRAME_SIZE	13
 #define MAXSIZE		16384
 
-unsigned char GETCONFIG[]	   = { 0x00, 0x40, 0x03, 0x00, 0x08 };
-unsigned char GETCONFIG_DATA[]	   = { 0x00, 0x42, 0x03, 0x00, 0x08 };
+unsigned char GETCONFIG[]          = { 0x00, 0x40, 0x03, 0x00, 0x08 };
+unsigned char GETCONFIG_DATA[]     = { 0x00, 0x42, 0x03, 0x00, 0x08 };
 unsigned char GETCONFIG_RESPONSE[] = { 0x00, 0x42, 0x03, 0x00, 0x06 };
+
+char *configs[][2] = {
+    {"loks", "lokomotive.cs2"},
+    {"mags", "magnetartikel.cs2"},
+    {"gbs", "gleisbild.cs2"},
+    {"fs", "fahrstrassen.cs2"},
+/*    {NULL, NULL}, */
+    {"lokstat", "lokomotive.sr2"},
+    {"magstat", "magnetartikel.sr2"},
+    {"gbsstat", "gbsstat.sr2"},
+    {"fsstat", "fahrstarssen.sr2"},
+    {NULL, NULL},
+};
 
 struct config_data {
     int deflated_stream_size;
@@ -35,6 +48,7 @@ struct config_data {
     int verbose;
     uint16_t crc;
     char *name;
+    char *filename;
     uint8_t *deflated_data;
     uint8_t *inflated_data;
 };
@@ -85,20 +99,22 @@ int config_write(struct config_data *config_data) {
 
     crc = CRCCCITT(config_data->deflated_data, config_data->deflated_stream_size, 0xFFFF);
 
-    printf("\n  writing to %s - size 0x%04x crc 0x%04x 0x%04x\n", config_data->name, config_data->deflated_stream_size,
-	   config_data->crc, crc);
+    printf("\n  writing to %s - size 0x%04x crc 0x%04x 0x%04x\n", config_data->filename,
+	   config_data->deflated_stream_size, config_data->crc, crc);
 
-    config_fp = fopen(config_data->name, "wb");
+    config_fp = fopen(config_data->filename, "wb");
     if (!config_fp) {
-	fprintf(stderr, "\ncan't open file %s for writing - error: %s\n", config_data->name, strerror(errno));
+	fprintf(stderr, "\ncan't open file %s for writing - error: %s\n", config_data->filename, strerror(errno));
 	exit(EXIT_FAILURE);
     } else {
-	for (i = 0; i < config_data->deflated_stream_size; i++) {
-	    if ((i % 8) == 0)
-		printf("\n");
-	    printf("%02x ", config_data->deflated_data[i]);
+	if (config_data->verbose) {
+	    for (i = 0; i < config_data->deflated_stream_size; i++) {
+		if ((i % 8) == 0)
+		    printf("\n");
+		printf("%02x ", config_data->deflated_data[i]);
+	    }
+	    printf("\n");
 	}
-	printf("\n");
     }
     inflate_data(config_data);
     fwrite(config_data->inflated_data, 1, config_data->inflated_size, config_fp);
@@ -114,27 +130,12 @@ int main(int argc, char **argv) {
     fd_set rset;
     unsigned char netframe[FRAME_SIZE];
     unsigned char recvline[MAXSIZE];
+    int config_index, file_not_done;
 
     if (argc != 3) {
 	fprintf(stderr, "usage: %s <dir_to_write> <IP address>\n", argv[0]);
 	exit(EXIT_FAILURE);
     }
-
-#if 0
-    if (strlen(argv[1]) > 7) {
-	fprintf(stderr, "config name to long\n");
-	exit(EXIT_FAILURE);
-    } else {
-	if ((config_data.name = malloc(strlen(argv[1] + 4)))) {
-	    config_data.name[0] = '\0';
-	    strcat(config_data.name, argv[1]);
-	    strcat(config_data.name, ".cs2");
-	} else {
-	    fprintf(stderr, "can't malloc config %s.z file name\n", argv[1]);
-	    exit(EXIT_FAILURE);
-	}
-    }
-#endif
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 	fprintf(stderr, "can't create TCP socket: %s\n", strerror(errno));
@@ -155,80 +156,93 @@ int main(int argc, char **argv) {
 	exit(EXIT_FAILURE);
     }
 
-    memset(netframe, 0, FRAME_SIZE);
-    memcpy(netframe, GETCONFIG, 5);
-    memcpy(&netframe[5], argv[1], strlen(argv[1]));
-    if (netframe_to_net(sockfd, netframe, FRAME_SIZE)) {
-	fprintf(stderr, "can't send data on TCP socket: %st\n", strerror(errno));
-	exit(EXIT_FAILURE);
-    }
+    config_data.verbose = 0;
+    config_index = 0;
+    while (configs[config_index][0]) {
+	config_data.name = configs[config_index][0];
+	config_data.filename = configs[config_index++][1];
 
-    FD_ZERO(&rset);
-    tcp_packet_nr = 0;
-    config_data_start = 0;
-    config_data_stream = 0;
-
-    for (;;) {
-	FD_SET(sockfd, &rset);
-
-	if (select(sockfd + 1, &rset, NULL, NULL, NULL) < 0) {
-	    fprintf(stderr, "socket error: %s\n", strerror(errno));
+	memset(netframe, 0, FRAME_SIZE);
+	memcpy(netframe, GETCONFIG, 5);
+	memcpy(&netframe[5], config_data.name, strlen(config_data.name));
+	if (netframe_to_net(sockfd, netframe, FRAME_SIZE)) {
+	    fprintf(stderr, "can't send data on TCP socket: %st\n", strerror(errno));
 	    exit(EXIT_FAILURE);
 	}
-	tcp_packet_nr++;
-	if (FD_ISSET(sockfd, &rset)) {
-	    if ((n = recv(sockfd, recvline, MAXSIZE, 0)) > 0) {
-		if (memcmp(recvline, GETCONFIG_RESPONSE, 5) == 0) {
-		    memcpy(&temp, &recvline[5], 4);
-		    deflated_size = ntohl(temp);
-		    config_data.deflated_size = deflated_size;
-		    memcpy(&temp, &recvline[9], 2);
-		    config_data.crc = ntohs(temp);
-		    printf("\nstart of config - deflated size: 0x%08x crc 0x%04x", deflated_size, config_data.crc);
-		    config_data_start = 1;
-		    /* we alloc 8 bytes more to be sure that it fits */
-		    config_data.deflated_data = malloc(deflated_size + 16);
-		    if (config_data.deflated_data == NULL) {
-			fprintf(stderr, "can't malloc deflated config data buffer - size 0x%04x\n", deflated_size + 8);
-			exit(EXIT_FAILURE);
+
+	FD_ZERO(&rset);
+	tcp_packet_nr = 0;
+	config_data_start = 0;
+	config_data_stream = 0;
+
+	file_not_done = 1;
+
+	while (file_not_done) {
+	    FD_SET(sockfd, &rset);
+
+	    if (select(sockfd + 1, &rset, NULL, NULL, NULL) < 0) {
+		fprintf(stderr, "socket error: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	    }
+	    tcp_packet_nr++;
+	    if (FD_ISSET(sockfd, &rset)) {
+		if ((n = recv(sockfd, recvline, MAXSIZE, 0)) > 0) {
+		    if (memcmp(recvline, GETCONFIG_RESPONSE, 5) == 0) {
+			memcpy(&temp, &recvline[5], 4);
+			deflated_size = ntohl(temp);
+			config_data.deflated_size = deflated_size;
+			memcpy(&temp, &recvline[9], 2);
+			config_data.crc = ntohs(temp);
+			printf("\nstart of config - deflated size: 0x%08x crc 0x%04x", deflated_size, config_data.crc);
+			config_data_start = 1;
+			/* we alloc 8 bytes more to be sure that it fits */
+			config_data.deflated_data = malloc(deflated_size + 16);
+			if (config_data.deflated_data == NULL) {
+			    fprintf(stderr, "can't malloc deflated config data buffer - size 0x%04x\n",
+				    deflated_size + 8);
+			    exit(EXIT_FAILURE);
+			}
+			/* deflated data index */
+			ddi = 0;
 		    }
-		    /* deflated data index */
-		    ddi = 0;
-		}
-		for (i = 0; i < n; i++) {
-		    if ((i % FRAME_SIZE) == 0) {
-			if (memcmp(&recvline[i], GETCONFIG_DATA, 5) == 0) {
-			    memcpy(&config_data.deflated_data[ddi], &recvline[i + 5], 8);
-			    ddi += 8;
-			    if (config_data_start) {
-				memcpy(&temp, &recvline[i + 5], 4);
-				config_data.inflated_size = ntohl(temp);
-				printf("\ninflated size: 0x%08x", config_data.inflated_size);
-				config_data.inflated_data = malloc(config_data.inflated_size);
-				if (config_data.inflated_data == NULL) {
-				    fprintf(stderr, "can't malloc inflated config data buffer - size 0x%04x\n",
-					    config_data.inflated_size);
-				    exit(EXIT_FAILURE);
-				}
-				config_data_start = 0;
-				config_data_stream = 1;
-				deflated_size -= 8;
-			    } else if (config_data_stream) {
-				if (deflated_size <= 8) {
-				    config_data_stream = 0;
-				    config_data.deflated_stream_size = ddi;
-				    config_write(&config_data);
-				    exit(EXIT_SUCCESS);
-				} else {
+		    for (i = 0; i < n; i++) {
+			if ((i % FRAME_SIZE) == 0) {
+			    if (memcmp(&recvline[i], GETCONFIG_DATA, 5) == 0) {
+				memcpy(&config_data.deflated_data[ddi], &recvline[i + 5], 8);
+				ddi += 8;
+				if (config_data_start) {
+				    memcpy(&temp, &recvline[i + 5], 4);
+				    config_data.inflated_size = ntohl(temp);
+				    printf("\ninflated size: 0x%08x", config_data.inflated_size);
+				    config_data.inflated_data = malloc(config_data.inflated_size);
+				    if (config_data.inflated_data == NULL) {
+					fprintf(stderr, "can't malloc inflated config data buffer - size 0x%04x\n",
+						config_data.inflated_size);
+					exit(EXIT_FAILURE);
+				    }
+				    config_data_start = 0;
+				    config_data_stream = 1;
 				    deflated_size -= 8;
+				} else if (config_data_stream) {
+				    if (deflated_size <= 8) {
+					config_data_stream = 0;
+					config_data.deflated_stream_size = ddi;
+					config_write(&config_data);
+					file_not_done = 0;
+				    } else {
+					deflated_size -= 8;
+				    }
 				}
 			    }
+			    if (config_data.verbose)
+				printf("\n %04d: ", tcp_packet_nr);
 			}
-			printf("\n %04d: ", tcp_packet_nr);
+			if (config_data.verbose)
+			    printf("%02x ", recvline[i]);
 		    }
-		    printf("%02x ", recvline[i]);
+		    if (config_data.verbose)
+			printf("\n");
 		}
-		printf("\n");
 	    }
 	}
     }
