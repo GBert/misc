906796d3edf017eefedafdb5efc27bf91092e92b
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2016-07-17 19:19:34 +0200

    wip

--- can2udp/src/clone-cs2-trigger.c
+++ can2udp/src/clone-cs2-trigger.c
@@ -7,12 +7,19 @@
  * ----------------------------------------------------------------------------
  */
 
+/*  LED		PI14	270
+ *  switch CE0	PI10	266
+ */
+
 #include <errno.h>
+#include <fcntl.h>
 #include <ifaddrs.h>
 #include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <pthread.h>
 #include <unistd.h>
 #include <time.h>
 #include <sys/time.h>
@@ -29,9 +36,13 @@
 #define MINDELAY	1000000	/* min delay in usec */
 #define MAXIPLEN	40	/* maximum IP string length */
 #define UDPPORT		15731
+#define MAX_BUFFER	8
+#define MAX_SYSFS_LEN	256
 
+unsigned int led_period;
+pthread_mutex_t lock;
 
-unsigned char CLONE_CONFIG_REQUEST[]     = { 0x00, 0x40, 0xaf, 0x7e, 0x00 };
+unsigned char CLONE_CONFIG_REQUEST[] = { 0x00, 0x40, 0xaf, 0x7e, 0x00 };
 
 struct trigger_t {
     struct sockaddr_in baddr;
@@ -39,17 +50,22 @@ struct trigger_t {
     int background;
     int verbose;
     int interval;
-    int offset;
+    int led_pin;
+    int switch_pin;
+    int led_fd;
+    int switch_fd;
     uint16_t hash;
     uint16_t hw_id;
 };
 
 void usage(char *prg) {
-    fprintf(stderr, "\nUsage: %s -vf [-b <bcast_addr/int>][-i <0|1>][-p <port>][-m <triggermodules>][-o <offset>]\n", prg);
+    fprintf(stderr, "\nUsage: %s -vf [-b <bcast_addr/int>][-i <sec>][-p <port>]\n", prg);
     fprintf(stderr, "   Version 1.0\n\n");
     fprintf(stderr, "         -b <bcast_addr/int> broadcast address or interface - default 255.255.255.255/br-lan\n");
     fprintf(stderr, "         -i <interval in sec>   using timer in sec\n");
     fprintf(stderr, "         -p <port>           destination port of the server - default %d\n", UDPPORT);
+    fprintf(stderr, "         -l                  LED pin (e.g. BPi PI14 -> 270)\n");
+    fprintf(stderr, "         -s                  switch pin (e.g. BPi PI10 -> 266)\n");
     fprintf(stderr, "         -f                  run in foreground (for debugging)\n");
     fprintf(stderr, "         -v                  be verbose\n\n");
 }
@@ -96,6 +112,133 @@ void print_net_frame(unsigned char *netframe) {
     printf("\n");
 }
 
+int gpio_export(int pin) {
+    char buffer[MAX_BUFFER];
+    ssize_t bytes_written;
+    int fd;
+
+    fd = open("/sys/class/gpio/export", O_WRONLY);
+    if (fd < 0) {
+	fprintf(stderr, "Failed to open export for writing: %s\n", strerror(errno));
+	return (EXIT_FAILURE);
+    }
+
+    bytes_written = snprintf(buffer, MAX_BUFFER, "%d", pin);
+    write(fd, buffer, bytes_written);
+    close(fd);
+    return (0);
+}
+
+int gpio_unexport(int pin) {
+    char buffer[MAX_BUFFER];
+    ssize_t bytes_written;
+    int fd;
+
+    fd = open("/sys/class/gpio/unexport", O_WRONLY);
+    if (fd < 0) {
+	fprintf(stderr, "Failed to open unexport for writing %s\n", strerror(errno));
+	return (EXIT_FAILURE);
+    }
+
+    bytes_written = snprintf(buffer, MAX_BUFFER, "%d", pin);
+    write(fd, buffer, bytes_written);
+    close(fd);
+    return (0);
+}
+
+int gpio_direction(int pin, int dir) {
+    char path[MAX_SYSFS_LEN];
+    int fd, ret;
+
+    snprintf(path, MAX_SYSFS_LEN, "/sys/class/gpio/gpio%d/direction", pin);
+    fd = open(path, O_WRONLY);
+    if (fd < 0) {
+	fprintf(stderr, "Failed to open gpio direction for writing %s\n", strerror(errno));
+	return (-1);
+    }
+
+    if (dir)
+	ret = write(fd, "in", 3);
+    else
+	ret = write(fd, "out", 4);
+
+    if (ret == -1) {
+	fprintf(stderr, "Failed to set direction!\n");
+	return (-1);
+    }
+
+    close(fd);
+    return (0);
+}
+
+int gpio_read(int pin) {
+    char path[MAX_SYSFS_LEN];
+    char value_str[3];
+    int fd, ret;
+
+    snprintf(path, MAX_SYSFS_LEN, "/sys/class/gpio/gpio%d/value", pin);
+    fd = open(path, O_RDONLY);
+    if (fd < 0) {
+	fprintf(stderr, "Failed to open gpio value for reading: %s\n", strerror(errno));
+	return (EXIT_FAILURE);
+    }
+
+    ret = read(fd, value_str, 3);
+    if (ret < 0) {
+	fprintf(stderr, "Failed to read value!\n");
+	return (EXIT_FAILURE);
+    }
+
+    close(fd);
+
+    return (atoi(value_str));
+}
+
+int gpio_set(int pin, int value) {
+    char path[MAX_SYSFS_LEN];
+    int fd, ret;
+
+    snprintf(path, MAX_SYSFS_LEN, "/sys/class/gpio/gpio%d/value", pin);
+    fd = open(path, O_WRONLY);
+    if (fd < 0) {
+	fprintf(stderr, "Failed to open gpio value for writing!\n");
+	return (EXIT_FAILURE);
+    }
+
+    if (value)
+	ret = write(fd, "1", 1);
+    else
+	ret = write(fd, "0", 1);
+
+    if (ret != 1) {
+	fprintf(stderr, "Failed to write value!\n");
+	return (EXIT_FAILURE);
+    }
+
+    close(fd);
+    return (0);
+}
+
+/* Blink LED */
+void *LEDMod(int led_period, int pin) {
+    int fd;
+    char path[MAX_SYSFS_LEN];
+
+    snprintf(path, MAX_SYSFS_LEN, "/sys/class/gpio/gpio%d/value", pin);
+    fd = open(path, O_WRONLY);
+    if (fd < 0) {
+	fprintf(stderr, "Failed to open gpio value for writing!\n");
+	exit(1);
+    }
+
+    while (1) {
+	write(fd, "1", 2);
+	usec_sleep(led_period);
+	write(fd, "0", 2);
+	usec_sleep(led_period);
+    }
+}
+
 int create_event(struct trigger_t *trigger) {
     int s;
     uint8_t netframe[13];
@@ -114,6 +257,7 @@ int create_event(struct trigger_t *trigger) {
 }
 
 int main(int argc, char **argv) {
+    pthread_t pth;
     int opt, ret, destination_port;
     struct sockaddr_in destaddr, *bsa;
     struct ifaddrs *ifap, *ifa;
@@ -122,7 +266,9 @@ int main(int argc, char **argv) {
     char *bcast_interface;
 
     const int on = 1;
-    trigger_data.interval= 0;
+    memset(&trigger_data, 0, sizeof(trigger_data));
+    trigger_data.led_pin = -1;
+    trigger_data.switch_pin = -1;
 
     destination_port = UDPPORT;
 
@@ -143,7 +289,7 @@ int main(int argc, char **argv) {
     destaddr.sin_family = AF_INET;
     destaddr.sin_port = htons(destination_port);
 
-    while ((opt = getopt(argc, argv, "b:i:p:fvh?")) != -1) {
+    while ((opt = getopt(argc, argv, "b:l:i:p:s:fvh?")) != -1) {
 	switch (opt) {
 	case 'b':
 	    if (strnlen(optarg, MAXIPLEN) <= MAXIPLEN - 1) {
@@ -163,9 +309,15 @@ int main(int argc, char **argv) {
 	case 'i':
 	    trigger_data.interval = atoi(optarg);
 	    break;
+	case 'l':
+	    trigger_data.led_pin = atoi(optarg);
+	    break;
 	case 'p':
 	    destination_port = strtoul(optarg, (char **)NULL, 10);
 	    destaddr.sin_port = htons(destination_port);
+	    break;
+	case 's':
+	    trigger_data.switch_pin = atoi(optarg);
 	    break;
 	case 'v':
 	    trigger_data.verbose = 1;
@@ -241,7 +393,7 @@ int main(int argc, char **argv) {
 
     /* loop forever */
     while (1) {
-        ret = create_event(&trigger_data);
+	ret = create_event(&trigger_data);
 	if (ret < 0) {
 	    fprintf(stderr, "problem sending event data - terminating\n");
 	    exit(EXIT_FAILURE);
