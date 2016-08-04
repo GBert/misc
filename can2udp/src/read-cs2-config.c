b1474be951451d1535648fee443dc6af4efcb6b5
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2016-08-04 20:45:48 +0200

    indent

--- can2udp/src/read-cs2-config.c
+++ can2udp/src/read-cs2-config.c
@@ -71,55 +71,55 @@ int read_track_config(struct track_config_t *config_data, char *config_file) {
 	    if (line[strlen(line) - 1] == '\n')
 		line[strlen(line) - 1] = 0;
 	    if (line[0] != ' ') {
-	        l01_token_n = get_char_index(l01_token, line);
+		l01_token_n = get_char_index(l01_token, line);
 		if (l01_token_n == L1_PAGE) {
 		    gbs_valid = 1;
-		    config_data->id  = 0;
+		    config_data->id = 0;
 		    printf("match seite:   >%s<\n", line);
 		}
 	    } else {
 		l2_token_n = get_char_index2(l2_token, line);
 		switch (l2_token_n) {
-		    case L2_ID:
-			config_data->id  = strtoul(&line[strlen(l2_token[l2_token_n])], NULL, 0);
-			printf("match id:      >%d<\n", config_data->id);
-			break;
-		    case L2_XOFFSET:
-			config_data->xoffset  = strtoul(&line[strlen(l2_token[l2_token_n])], NULL, 0);
-			printf("match xoffset: >%d<\n", config_data->xoffset);
-			break;
-		    case L2_YOFFSET:
-			config_data->yoffset  = strtoul(&line[strlen(l2_token[l2_token_n])], NULL, 0);
-			printf("match yoffset: >%d<\n", config_data->yoffset);
-			break;
-		    case L2_MAJOR:
-			config_data->major = strtoul(&line[strlen(l2_token[l2_token_n])], NULL, 0);
-			printf("match major:   >%d<\n", config_data->major);
-			break;
-		    case L2_MINOR:
-			config_data->minor = strtoul(&line[strlen(l2_token[l2_token_n])], NULL, 0);
-			printf("match minor:   >%d<\n", config_data->minor);
-			break;
-		    case L2_TYPE:
-			printf("match typ:     >%s<\n", line);
-			ret = get_char_index(track_types, &line[strlen(l2_token[l2_token_n])]);
-			if (ret >= 0) {
-			    printf("hit  %d -> %s\n", ret, track_types[ret]);
-			} else {
-			    printf("miss %d\n", ret);
-			}
-			break;
-		    case L2_NAME:
-			printf("match name:    >%s<\n", line);
-			if (gbs_valid) {
-			    strncpy(gbs_name, &line[7], strlen(&line[7]));
-			    strcat(gbs_name, ".cs2");
-			    config_data->name = gbs_name;
-			}
-			break;
-		    default:
-			printf("unknown:       >%s<\n", line);
-			break;
+		case L2_ID:
+		    config_data->id = strtoul(&line[strlen(l2_token[l2_token_n])], NULL, 0);
+		    printf("match id:      >%d<\n", config_data->id);
+		    break;
+		case L2_XOFFSET:
+		    config_data->xoffset = strtoul(&line[strlen(l2_token[l2_token_n])], NULL, 0);
+		    printf("match xoffset: >%d<\n", config_data->xoffset);
+		    break;
+		case L2_YOFFSET:
+		    config_data->yoffset = strtoul(&line[strlen(l2_token[l2_token_n])], NULL, 0);
+		    printf("match yoffset: >%d<\n", config_data->yoffset);
+		    break;
+		case L2_MAJOR:
+		    config_data->major = strtoul(&line[strlen(l2_token[l2_token_n])], NULL, 0);
+		    printf("match major:   >%d<\n", config_data->major);
+		    break;
+		case L2_MINOR:
+		    config_data->minor = strtoul(&line[strlen(l2_token[l2_token_n])], NULL, 0);
+		    printf("match minor:   >%d<\n", config_data->minor);
+		    break;
+		case L2_TYPE:
+		    printf("match typ:     >%s<\n", line);
+		    ret = get_char_index(track_types, &line[strlen(l2_token[l2_token_n])]);
+		    if (ret >= 0) {
+			printf("hit  %d -> %s\n", ret, track_types[ret]);
+		    } else {
+			printf("miss %d\n", ret);
+		    }
+		    break;
+		case L2_NAME:
+		    printf("match name:    >%s<\n", line);
+		    if (gbs_valid) {
+			strncpy(gbs_name, &line[7], strlen(&line[7]));
+			strcat(gbs_name, ".cs2");
+			config_data->name = gbs_name;
+		    }
+		    break;
+		default:
+		    printf("unknown:       >%s<\n", line);
+		    break;
 		}
 	    }
 	}
@@ -157,7 +157,7 @@ int main(int argc, char **argv) {
 
     track_file = calloc(strlen(track_name) + strlen(config_data.directory) + 2, 1);
     strcpy(track_file, config_data.directory);
-    if (track_file[strlen(track_file)-1] != '/')
+    if (track_file[strlen(track_file) - 1] != '/')
 	strcat(track_file, "/");
     strcat(track_file, track_name);
 
