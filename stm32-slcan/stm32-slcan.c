ac12c8fb7b104c133780822125b0d34d18732705
eh2be5j <info@gerhard-bertelsmann.de>
2016-09-12 13:52:05 +0200

    wip

--- stm32-slcan/stm32-slcan.c
+++ stm32-slcan/stm32-slcan.c
@@ -11,12 +11,7 @@
  * This file is derived from the libopencm3 project examples 
  */
 
-#include <libopencm3/cm3/nvic.h>
-#include <libopencm3/cm3/systick.h>
-#include <libopencm3/stm32/can.h>
-#include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/flash.h>
-#include <libopencm3/stm32/rcc.h>
+#include "stm32-slcan.h"
 
 volatile unsigned int counter;
 volatile uint8_t status;
@@ -33,6 +28,10 @@ static void gpio_setup(void) {
 
     /* Configure LED GPIO */
     gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+
+    /* Enable clocks for GPIO port A (for GPIO_USART2_TX) and USART2. */
+   rcc_periph_clock_enable(RCC_AFIO);
+   rcc_periph_clock_enable(RCC_USART2);
 }
 
 static void systick_setup(void) {
@@ -49,30 +48,70 @@ static void systick_setup(void) {
     systick_counter_enable();
 }
 
-static void can_speed(int index) {
-    /*	TCM: Time triggered comm mode?
+static int can_speed(int index) {
+    int ret;
+
+    /*
+	S0 = 10 kBaud
+	S1 = 20 kBaud
+	S2 = 50 kBaud
+	S3 = 100 kBaud
+	S4 = 125 kBaud
+	S5 = 250 kBaud
+	S6 = 500 kBaud
+	S7 = 800 kBaud
+	S8 = 1 MBaud
+
+	  10 kBaud Sample Point 75.0 % : 36MHZ / 180 = 200kHz -> TQ 20   SJW + TS1 15 TS2 5
+	  20 kBaud Sample Point 75.0 % : 36MHZ /  90 = 400kHz -> TQ 20   SJW + TS1 15 TS2 5
+	  50 kBaud Sample Point 75.0 % : 36MHZ /  36 = 1MHz   -> TQ 20   SJW + TS1 15 TS2 5
+	 100 kBaud Sample Point 80.0 % : 36MHZ /  36 = 1MHz   -> TQ 10   SJW + TS1  8 TS2 2
+	 125 kBaud Sample Point 87.5 % : 36MHZ /  18 = 2MHz   -> TQ 16   SJW + TS1 14 TS2 2 
+	 250 kBaud Sample Point 87.5 % : 36MHZ /   9 = 4MHz   -> TQ 16   SJW + TS1 14 TS2 2
+	 500 kBaud Sample Point 87.5 % : 36MHZ /   9 = 4MHz   -> TQ  8   SJW + TS1  7 TS2 1
+	 800 kBaud Sample Point 86.7 % : 36MHZ /   3 = 12MHz  -> TQ 15   SJW + TS1 13 TS2 2
+	1000 kBaud Sample Point 88.9 % : 36MHZ /   2 = 18MHz  -> TQ 18   SJW + TS1 16 TS2 2
+
+    	TTCM: Time triggered comm mode
 	ABOM: Automatic bus-off management
 	AWUM: Automatic wakeup mode
 	NART: No automatic retransmission
 	RFLM: Receive FIFO locked mode
 	TXFP: Transmit FIFO priority
      */
-    /* CAN cell init.
-       Setting the bitrate to 250kHz. APB1 = 36MHz, 
-       prescaler = 9 -> 4MHz time quanta frequency.
-       1tq sync + 9tq bit segment1 (TS1) + 6tq bit segment2 (TS2) = 
-       16time quanto per bit period, therefor 4MHz/16 = 250kHz
-     */
-
-    if (index < 10) {
-	switch(index) {
-	case 8: can_init(CAN1, false, true, false, false, false, false,
-			CAN_BTR_SJW_1TQ, CAN_BTR_TS1_9TQ, CAN_BTR_TS2_6TQ, 9, false, false);
-		break;
-	default:
-		break;
-	}
+    switch(index) {
+    case 0: ret = can_init(CAN1, false, true, false, false, false, false,
+			CAN_BTR_SJW_1TQ, CAN_BTR_TS1_14TQ, CAN_BTR_TS2_5TQ, 180, false, false);
+	break;
+    case 1: ret = can_init(CAN1, false, true, false, false, false, false,
+ 			CAN_BTR_SJW_1TQ, CAN_BTR_TS1_14TQ, CAN_BTR_TS2_5TQ,  90, false, false);
+	break;
+    case 2: ret = can_init(CAN1, false, true, false, false, false, false,
+			CAN_BTR_SJW_1TQ, CAN_BTR_TS1_14TQ, CAN_BTR_TS2_5TQ,  36, false, false);
+	break;
+    case 3: ret = can_init(CAN1, false, true, false, false, false, false,
+			CAN_BTR_SJW_1TQ, CAN_BTR_TS1_8TQ, CAN_BTR_TS2_2TQ,   36, false, false);
+	break;
+    case 4: ret = can_init(CAN1, false, true, false, false, false, false,
+			CAN_BTR_SJW_1TQ, CAN_BTR_TS1_13TQ, CAN_BTR_TS2_2TQ,  18, false, false);
+	break;
+    case 5: ret = can_init(CAN1, false, true, false, false, false, false,
+			CAN_BTR_SJW_1TQ, CAN_BTR_TS1_13TQ, CAN_BTR_TS2_2TQ,   9, false, false);
+	break;
+    case 6: ret = can_init(CAN1, false, true, false, false, false, false,
+			CAN_BTR_SJW_1TQ, CAN_BTR_TS1_7TQ, CAN_BTR_TS2_1TQ,    9, false, false);
+	break;
+    case 7: ret = can_init(CAN1, false, true, false, false, false, false,
+			CAN_BTR_SJW_1TQ, CAN_BTR_TS1_12TQ, CAN_BTR_TS2_2TQ,   3, false, false);
+	break;
+    case 8: ret = can_init(CAN1, false, true, false, false, false, false,
+			CAN_BTR_SJW_1TQ, CAN_BTR_TS1_15TQ, CAN_BTR_TS2_2TQ,    2, false, false);
+	break;
+    default:
+	ret = -1;
+	break;
     }
+    return ret;
 }
 
 static void can_setup(void) {
@@ -103,6 +142,7 @@ static void can_setup(void) {
      * 1tq sync + 9tq bit segment1 (TS1) + 6tq bit segment2 (TS2) = 
      * 16time quanto per bit period, therefor 4MHz/16 = 250kHz
      */
+#if 0
     if (can_init(CAN1,
 		 false,		/* TTCM: Time triggered comm mode? */
 		 true,		/* ABOM: Automatic bus-off management? */
@@ -116,6 +156,8 @@ static void can_setup(void) {
 		 9,
 		 false,
 		 false))	{
+#endif
+    if (can_speed(5)) {
 	gpio_clear(GPIOC, GPIO13);	/* LED green on */
 
 	/* Die because we failed to initialize. */
---
+++ stm32-slcan/stm32-slcan.h
@@ -0,0 +1,22 @@
+/* ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <info@gerhard-bertelsmann.de> wrote this file. As long as you retain this
+ * notice you can do whatever you want with this stuff. If we meet some day,
+ * and you think this stuff is worth it, you can buy me a beer in return
+ * Gerhard Bertelsmann
+ * ----------------------------------------------------------------------------
+ */
+
+#ifndef _STM32_SLCAN_H_
+#define _STM32_SLCAN_H_
+
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/stm32/can.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/flash.h>
+#include <libopencm3/stm32/rcc.h>
+#include <stdio.h>
+#include <errno.h>
+
+#endif  /* _STM32_SLCAN_H_ */
