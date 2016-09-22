ab5e3800d4d98f3938e934eb2028cc76973cdde9
eh2be5j <info@gerhard-bertelsmann.de>
2016-09-22 12:06:35 +0200

    be more generic

--- rocrail/make-rocrail.sh
+++ rocrail/make-rocrail.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
 
+TMPDIR=/tmp
+HOME=/home/gerd/projekte
+LEDEDIR=${HOME}/lede
+
 set -x
-cd /tmp
-tar Jxf ~/projekte/rocrail.tar.xz
+cd ${TMPDIR}
+tar Jxf ${HOME}/rocrail.tar.xz
 cd Rocrail
 git pull
 cd ..
-mv ~/projekte/rocrail.tar.xz ~/projekte/rocrail.tar.xz_save
-tar Jcf ~/projekte/rocrail.tar.xz Rocrail
+mv ${HOME}/rocrail.tar.xz ${HOME}/rocrail.tar.xz_save
+tar Jcf ${HOME}/rocrail.tar.xz Rocrail
 cd Rocrail
 make version
 
 REVISION=`git rev-list --count HEAD`
 VERSION=`git log -n 1 --pretty=format:%H | cut -b-10`
 
-echo "$REVISION $VERSION"
-
-cp common/version.h /home/gerd/projekte/lede/feeds/railroad/rocrail/common
+cp common/version.h ${LEDEDIR}/feeds/railroad/rocrail/common
 rm -rf .cdtproject .cproject .git .project .settings
 cd ..
-sed "s/__VERSION__/${REVISION}/" ~/projekte/Makefile.rocrail > Makefile.r1
-sed "s/__REVISION__/${VERSION}/" Makefile.r1 > /home/gerd/projekte/lede/feeds/railroad/rocrail/Makefile
+sed "s/__VERSION__/${REVISION}/" ${HOME}/Makefile.rocrail > Makefile.r1
+sed "s/__REVISION__/${VERSION}/" Makefile.r1 > ${LEDEDIR}/feeds/railroad/rocrail/Makefile
 mv Rocrail rocrail
 tar zcf rocrail-${REVISION}.tar.gz rocrail
-mv rocrail-${REVISION}.tar.gz /home/gerd/projekte/lede/dl/
+mv rocrail-${REVISION}.tar.gz ${LEDEDIR}/dl/
 rm -rf rocrail
-cd /home/gerd/projekte/lede/feeds/railroad/rocrail/
+cd ${LEDEDIR}/feeds/railroad/rocrail/
 git commit -a -m "bump revision" && git push
