beea8374ba47c43ecfc7a2bd458c21000745a923
GBert <info@gerhard-bertelsmann.de>
2016-10-30 23:10:40 +0100

    bump revision

--- rocrail/common/version.h
+++ rocrail/common/version.h
@@ -1,5 +1,58 @@
 const int revisionnr = 
-11512
+11580
 ;
 const char* commithash = 
-"258b4c7ab0b6f6d2301fc1078ed869f17b9bc93a";
+"62eeb44aec61593b4c33556791cdf64870bf0b07";
+const char* revlog = 
+"11580 2016-10-30 10:28:49 +0100 locdialog: do not show BBT from block if the use is not set to from block\n" \
+"11579 2016-10-30 09:20:57 +0100 item: fix for background color on paint\n" \
+"11578 2016-10-29 17:55:08 +0200 routedialog: fix for modifying existing route commands without type set\n" \
+"11577 2016-10-29 16:14:09 +0200 loc: check if the block fixed BBT is smaller then the loco BBT steps to avoid dividing by zero\n" \
+"11576 2016-10-29 15:18:40 +0200 mdi: support for moving items to another level\n" \
+"11575 2016-10-29 11:56:03 +0200 mdimanager: include typo fix\n" \
+"11574 2016-10-29 11:08:54 +0200 midichild: maximize and minimize boxes added\n" \
+"11573 2016-10-29 10:53:54 +0200 uhl68610: changed title to Lissy\n" \
+"11572 2016-10-29 10:46:58 +0200 lncv: removed the lncv tab option\n" \
+"11571 2016-10-29 08:28:13 +0200 translation merge: de, sl, pt_BR, nl\n" \
+"11570 2016-10-29 07:02:19 +0200 mdi: support for update model events\n" \
+"11569 2016-10-28 12:30:49 +0200 rocview: optional tab child nodes in the workspace (wip)\n" \
+"11568 2016-10-28 06:33:31 +0200 guiframe: only call onexit once at shutdown\n" \
+"11567 2016-10-27 14:57:02 +0200 guiframe: check if local plan is modified before opening another\n" \
+"11566 2016-10-27 11:51:20 +0200 routedialog: show preselected routes\n" \
+"11565 2016-10-27 10:38:09 +0200 mdi: delete panel support added\n" \
+"11564 2016-10-27 10:09:04 +0200 planpanel: added print popup menu\n" \
+"11563 2016-10-27 09:12:45 +0200 mdimanager: set plan title (for windows)\n" \
+"11562 2016-10-27 08:40:26 +0200 rocguiinidlg: added the MDI option\n" \
+"11561 2016-10-27 08:31:28 +0200 mdi: dispatch model events to the MDI manager to update all levels\n" \
+"11560 2016-10-26 16:02:14 +0200 mdimanager: save position and size of the mdi parent frame\n" \
+"11559 2016-10-26 15:24:50 +0200 mdimanager: no default menu to prevent an assert under windows\n" \
+"11558 2016-10-26 15:21:07 +0200 midimanager: create a tab node in case there is none for saving position and size\n" \
+"11557 2016-10-26 14:02:54 +0200 mdi: WIP\n" \
+"11556 2016-10-26 13:58:07 +0200 mdi: WIP\n" \
+"11555 2016-10-26 11:58:06 +0200 item: check at the blockstate event if the item is already initialized\n" \
+"11554 2016-10-26 11:47:10 +0200 mdi: manager class added\n" \
+"11553 2016-10-26 11:37:55 +0200 mdi: experimetal multi frame view\n" \
+"11552 2016-10-26 05:11:50 +0200 trackdialog: disable prev, next and index panel if its not filled\n" \
+"11551 2016-10-25 15:29:26 +0200 trackdialog: prev and next track added, rocview always fill index option added\n" \
+"11550 2016-10-25 11:05:19 +0200 rocweb: show signal aspect in the direction button\n" \
+"11549 2016-10-25 11:03:39 +0200 throttledlg: update the signalaspect attribute on model event\n" \
+"11548 2016-10-25 10:58:02 +0200 throttle: activate LED in the speed field to show red aspect\n" \
+"11547 2016-10-25 10:33:15 +0200 throttledlg: show signal aspect in the speed field\n" \
+"11546 2016-10-25 09:15:15 +0200 lc: set signal aspect in the speed field on loco selection\n" \
+"11545 2016-10-24 15:23:40 +0200 item: support for dubble click on curved connectors\n" \
+"11544 2016-10-24 13:35:22 +0200 revision history: revsion number added (Lothar)\n" \
+"11543 2016-10-24 09:19:46 +0200 infodialog: enable copyright field for scrolling\n" \
+"11542 2016-10-23 15:45:01 +0200 infodialog: tab translation added\n" \
+"11541 2016-10-23 15:34:48 +0200 item: add the color node in the output object if no existing is there\n" \
+"11540 2016-10-23 14:40:10 +0200 item: fix for modify outputs offline\n" \
+"11539 2016-10-23 14:19:34 +0200 rocnetnode makefile fix for revision log\n" \
+"11538 2016-10-23 14:07:51 +0200 makefile: revlog added\n" \
+"11537 2016-10-23 13:51:49 +0200 makefile: version info offline fix\n" \
+"11536 2016-10-23 12:11:59 +0200 infodlg: show last 50 commit comments\n" \
+"11535 2016-10-23 08:56:59 +0200 loc: xmlscript support for f0-f28 state\n" \
+"11534 2016-10-23 08:45:41 +0200 item: activate tab level of connector counter part at double click\n" \
+"11533 2016-10-22 10:39:49 +0200 routedialog: fix for modify command ID\n" \
+"11532 2016-10-22 09:33:17 +0200 optional signal events for locos added\n" \
+"11531 2016-10-21 12:31:32 +0200 dccpp: check if the serial object is still alive before read\n" \
+
+"log end";
