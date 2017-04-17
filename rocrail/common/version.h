c54410ba95533a94e6f168e40a4ca75d1e8b97b4
GBert <info@gerhard-bertelsmann.de>
2017-04-17 18:43:49 +0200

    bump revision

--- rocrail/common/version.h
+++ rocrail/common/version.h
@@ -1,9 +1,43 @@
 const int revisionnr = 
-12158
+12192
 ;
 const char* commithash = 
-"7baa13dad7c22ae8643985131d43717f49f0509e";
+"b10e2f8780bd3f636770c74580fb3a287107ffe5";
 const char* revlog = 
+"12192 2017-04-17 15:47:20 +0200 model: allow csv list for roadnames in startall filter\n" \
+"12191 2017-04-17 15:25:30 +0200 model: memory leak repared for writing occ.xml\n" \
+"12190 2017-04-17 10:47:28 +0200 startallfilterdlg: added main line option\n" \
+"12189 2017-04-17 10:02:38 +0200 start all filter dialog added\n" \
+"12188 2017-04-17 07:27:12 +0200 milight: revert white workaround\n" \
+"12187 2017-04-16 14:44:57 +0200 rocnetnode: listener interface corrected to the new version\n" \
+"12186 2017-04-16 14:03:21 +0200 clockdlg: save size and position on shutdown\n" \
+"12185 2017-04-16 13:30:24 +0200 clock: added missing include\n" \
+"12184 2017-04-16 13:06:57 +0200 clockdlg: open dialog at startup in case it was open at shutdown\n" \
+"12183 2017-04-16 12:24:13 +0200 clockdlg: layout correction\n" \
+"12182 2017-04-16 11:48:08 +0200 rocview: replaced the conflicting clock wrapper with fastclock\n" \
+"12181 2017-04-16 11:11:04 +0200 seltab: accessory interface option added\n" \
+"12180 2017-04-16 11:10:18 +0200 clockdlg: translations\n" \
+"12179 2017-04-16 10:42:43 +0200 clockdlg: save size and position\n" \
+"12178 2017-04-16 10:28:54 +0200 clockdlg: synclock added\n" \
+"12177 2017-04-16 10:24:00 +0200 clockdlg: resizeable fast clock\n" \
+"12176 2017-04-15 15:27:34 +0200 seltab: generate switch IDs for dccpp\n" \
+"12175 2017-04-15 12:05:43 +0200 check schedule time in case the secondnextblock4wait did reserve the next destination\n" \
+"12174 2017-04-15 11:05:08 +0200 basedlg: startup schedule and tour column added\n" \
+"12173 2017-04-15 10:59:20 +0200 esunavi: crash fix in case no serial device is initialised\n" \
+"12172 2017-04-15 09:38:22 +0200 roclcdr: save the secondnextblock4wait index in its own variable for adjusting the index after wait\n" \
+"12171 2017-04-14 17:38:45 +0200 model: only show warning if the schedule index is in range\n" \
+"12170 2017-04-14 16:32:04 +0200 secondnextblock options also in the operator\n" \
+"12169 2017-04-14 15:21:04 +0200 location, model: check if the blockid is valid\n" \
+"12168 2017-04-14 15:14:35 +0200 stage: unregister occ sensors too on delete and modify\n" \
+"12167 2017-04-14 11:35:25 +0200 z21: global railcom trace correction\n" \
+"12166 2017-04-14 10:58:45 +0200 reserve: schedule index fix for secondnextblock4wait\n" \
+"12165 2017-04-14 10:26:54 +0200 secondnextblock4wait: some corrections made in case of schedule\n" \
+"12164 2017-04-14 09:22:38 +0200 model: fix for deleting a deleted sensor from the list-map\n" \
+"12163 2017-04-13 17:10:27 +0200 block: remove listeners on delete\n" \
+"12162 2017-04-12 11:48:35 +0200 translation merge: sr_latin, de, pt_BR, nl\n" \
+"12161 2017-04-12 09:36:58 +0200 model: fix for possible endless loop in finding the right schedule index\n" \
+"12160 2017-04-12 08:47:47 +0200 initialize: show warning in case a block group could not be locked\n" \
+"12159 2017-04-12 08:19:41 +0200 bidib: handleConfigX extended\n" \
 "12158 2017-04-11 19:13:14 +0200 bidib: adressing fix for configx\n" \
 "12157 2017-04-11 11:13:13 +0200 model: if the schedule index does not match use it as offset to find the matching index\n" \
 "12156 2017-04-11 09:51:16 +0200 initdest: reset destination in case the block group clould not be locked\n" \
@@ -20,39 +54,5 @@ const char* revlog =
 "12145 2017-04-06 10:01:38 +0200 rocpro: outo detect structure changed\n" \
 "12144 2017-04-05 11:30:14 +0200 decodertypes: update\n" \
 "12143 2017-04-05 11:16:04 +0200 model: check for multiple signals at event\n" \
-"12142 2017-04-05 08:48:02 +0200 compiler warnings fixed\n" \
-"12141 2017-04-05 07:58:21 +0200 decodertypes: D&H added\n" \
-"12140 2017-04-04 16:59:14 +0200 guiframe: check on close if the car or train tab was already initialized before call th exit function\n" \
-"12139 2017-04-04 08:22:01 +0200 locdialog: added button to fix and center all BBT records\n" \
-"12138 2017-04-04 07:05:45 +0200 p50x: error code list added\n" \
-"12137 2017-04-03 15:28:34 +0200 rocprodlg: try to find the fitting decoder file\n" \
-"12136 2017-04-03 10:41:52 +0200 rocprodlg: detect decoder type\n" \
-"12135 2017-04-03 07:53:52 +0200 stage: fix for advancing in case of percent arrive speed\n" \
-"12134 2017-04-03 06:46:53 +0200 decodertypes: update\n" \
-"12133 2017-04-02 15:11:13 +0200 decodertypes: WIP\n" \
-"12132 2017-04-02 09:46:20 +0200 speedcurvedlg: slider height set to default -1\n" \
-"12131 2017-04-02 08:22:32 +0200 speedcurvedlg: layout changed\n" \
-"12130 2017-04-02 07:33:41 +0200 locdialog: hide and show all button added\n" \
-"12129 2017-04-01 13:06:12 +0200 guiframe: fix for first entry of open recent\n" \
-"12128 2017-04-01 09:07:15 +0200 cbusnodedlg: export/import for the cangc6\n" \
-"12127 2017-03-31 12:18:24 +0200 cbusnodedlg: exoport/import gc2\n" \
-"12126 2017-03-31 07:13:10 +0200 wmz21: swapped the switch command to resembel the display\n" \
-"12125 2017-03-30 10:56:25 +0200 block: bring warning in case of an empty or blank include and ignore the complete list\n" \
-"12124 2017-03-30 10:07:04 +0200 block: bring warning in case of an empty include and ignore the complete list\n" \
-"12123 2017-03-30 07:13:32 +0200 loc: fix for initial calculation of the train length in case of consist locos\n" \
-"12122 2017-03-30 06:38:00 +0200 dccpp: use parameter for the output port setting flag\n" \
-"12121 2017-03-30 06:09:00 +0200 locdialog: select the first cvbyte after a delete\n" \
-"12120 2017-03-29 17:57:04 +0200 locdialog: fix for deleting a CV\n" \
-"12119 2017-03-29 16:51:14 +0200 locdialog: check before cv delete if there are entries in the grid\n" \
-"12118 2017-03-29 10:17:19 +0200 translation merge: sr_latin, sv, de\n" \
-"12117 2017-03-29 09:42:42 +0200 loc: reset energypercentage on init and reset\n" \
-"12116 2017-03-29 07:56:11 +0200 locdialog: import CVs\n" \
-"12115 2017-03-28 10:32:51 +0200 model: splitup the init process into two parts, create and init\n" \
-"12114 2017-03-27 10:14:14 +0200 trainpanel: sorting columns added\n" \
-"12113 2017-03-27 08:31:48 +0200 wmz21: A2ID support added\n" \
-"12112 2017-03-27 07:53:44 +0200 wmz21: FADAM support added\n" \
-"12111 2017-03-26 15:26:48 +0200 dccpp: fix for getting a sw ID incase none accessory\n" \
-"12110 2017-03-26 10:46:15 +0200 loc: set bkid and lcclass at activating an action\n" \
-"12109 2017-03-26 10:16:46 +0200 wmz21: support for switch motor 2\n" \
 
 "log end";
