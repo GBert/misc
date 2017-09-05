388626460751ac6bad247339140e2a113fe6139e
GBert <info@gerhard-bertelsmann.de>
2017-09-05 10:03:25 +0200

    wip

--- RS485/stm32/main-stm32f103-generic.c
+++ RS485/stm32/main-stm32f103-generic.c
@@ -85,7 +85,7 @@ static void usart_setup(void) {
     rcc_periph_clock_enable(RCC_USART2);
 
     /* Setup USART2 parameters. */
-    usart_set_baudrate(USART2, 62500);
+    usart_set_baudrate(USART2, 50000);
     usart_set_databits(USART2, 9);
     usart_set_stopbits(USART2, USART_STOPBITS_1);
     usart_set_mode(USART2, USART_MODE_TX_RX);
@@ -137,7 +137,10 @@ void usart2_isr(void) {
 		cdcacm_arch_pin(0, CDCACM_PIN_RS485DE, 1);
 		de_set_first = 0;
 	    }
-	    uint16_t c = (((ringb_get(&tx_ring) << 8) & 0x100) | ringb_get(&tx_ring));
+
+	    // uint16_t c = (((ringb_get(&tx_ring) << 8) & 0x100) | ringb_get(&tx_ring));
+	    uint16_t c = ringb_get(&tx_ring);
+	    // c = 0x155;
 	    usart_send(USART2, c);
 	}
     }
