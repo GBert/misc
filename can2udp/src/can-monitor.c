c429213fe8855cd3dfd35b15662c5658f4570470
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2017-09-15 08:24:03 +0200

    add colors

--- can2udp/src/can-monitor.c
+++ can2udp/src/can-monitor.c
@@ -28,6 +28,15 @@
 #include <time.h>
 #include <linux/can.h>
 
+#define RED	"\x1B[31m"
+#define GRN	"\x1B[32m"
+#define YEL	"\x1B[33m"
+#define BLU	"\x1B[34m"
+#define MAG	"\x1B[35m"
+#define CYN	"\x1B[36m"
+#define WHT	"\x1B[37m"
+#define RESET	"\x1B[0m"
+
 #define MAXDG   	4096	/* maximum datagram size */
 #define MAXUDP  	16	/* maximum datagram size */
 #define MAX(a,b)	((a) > (b) ? (a) : (b))
@@ -54,11 +63,13 @@ const char *subCmdNames[] = {
 };
 
 void writeRed(const char *subCmdNames) {
-    printf("%s\n", subCmdNames);
+    printf(RED "%s\n", subCmdNames);
+    printf(RESET);
 }
 
 void writeGreen(const char *subCmdNames) {
-    printf("%s\n", subCmdNames);
+    printf(GRN "%s\n", subCmdNames);
+    printf(RESET);
 }
 
 void print_usage(char *prg) {
