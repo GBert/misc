311424815e21047d5cc0a83d7a2de481e4af5c4f
GBert <info@gerhard-bertelsmann.de>
2017-12-05 11:04:03 +0100

    bump revision

--- rocrail/common/version.h
+++ rocrail/common/version.h
@@ -1,9 +1,21 @@
 const int revisionnr = 
-13140
+13152
 ;
 const char* commithash = 
-"ffaf908270ba9e7a5ca2fcaa6cbaf1e3be89eb5a";
+"fe143caee31530a52df69781740c6fbaeb275e39";
 const char* revlog = 
+"13152 2017-12-05 08:56:24 +0100 control: set var lcid on event timeout\n" \
+"13151 2017-12-05 08:32:00 +0100 translation merge: de, sl, sr_latin, fr, en_GB, nl\n" \
+"13150 2017-12-04 17:36:15 +0100 trace: level char for analyser\n" \
+"13149 2017-12-04 16:41:03 +0100 analyse: added extra trace level and corrected trace level to avoid a flood of messages to the clients\n" \
+"13148 2017-12-04 14:37:25 +0100 makefile: copy rocctrl icon\n" \
+"13147 2017-12-04 14:32:46 +0100 rocctrl: own icon\n" \
+"13146 2017-12-04 14:14:08 +0100 rocctrl: informall flag for system commands added\n" \
+"13145 2017-12-04 13:21:00 +0100 rocctrl: menu and frame buttons equal text\n" \
+"13144 2017-12-04 13:07:20 +0100 bigger font for buttons\n" \
+"13143 2017-12-04 10:22:56 +0100 rocctrl: resume and virtual button added\n" \
+"13142 2017-12-04 10:18:07 +0100 rocctrl: automate frame added\n" \
+"13141 2017-12-03 22:01:14 +0100 rocctrl: quit on eod\n" \
 "13140 2017-12-03 17:21:51 +0100 routedialog: multi delete\n" \
 "13139 2017-12-03 17:00:28 +0100 rocctrl: clean up\n" \
 "13138 2017-12-03 15:15:09 +0100 rocctrl: messagebox for python 2 and 3\n" \
@@ -42,17 +54,5 @@ const char* revlog =
 "13105 2017-11-27 13:36:27 +0100 wait4event: release blockgroup on enter too (test)\n" \
 "13104 2017-11-27 11:54:51 +0100 routedialog: popup for removing all autogen with questionmark\n" \
 "13103 2017-11-27 11:17:42 +0100 routedialog: remove also tt generated flags\n" \
-"13102 2017-11-27 09:57:35 +0100 locdialog: prev function group from 1 to 8\n" \
-"13101 2017-11-27 08:12:53 +0100 loc: fix for function > 28 timers\n" \
-"13100 2017-11-26 14:38:57 +0100 mvtrack: reset command support added\n" \
-"13099 2017-11-26 13:52:02 +0100 fback: do not increase wheelcounter at command\n" \
-"13098 2017-11-26 08:48:13 +0100 rocnetnode: init outputs at startup\n" \
-"13097 2017-11-26 08:34:05 +0100 rocnetnode: check the new ini from SD before rename it to rocnetnode.ini\n" \
-"13096 2017-11-25 19:00:46 +0100 makewin64-full.sh: only one task on the raspi\n" \
-"13095 2017-11-25 18:13:47 +0100 roclcdr: free on enter/wheel count fixes\n" \
-"13094 2017-11-25 17:05:21 +0100 roclcdr: free on enter/wheel count fixes\n" \
-"13093 2017-11-25 15:09:49 +0100 rocnetnode: start of day fix\n" \
-"13092 2017-11-25 14:06:33 +0100 rocnetnode: SoD traces added\n" \
-"13091 2017-11-25 12:14:39 +0100 rocnetnode: trace errno text\n" \
 
 "log end";
