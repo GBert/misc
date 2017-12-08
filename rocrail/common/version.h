47ae4802393affb4f53f18800222cf2b1f949f05
GBert <info@gerhard-bertelsmann.de>
2017-12-08 15:34:20 +0100

    bump revision

--- rocrail/common/version.h
+++ rocrail/common/version.h
@@ -1,9 +1,27 @@
 const int revisionnr = 
-13163
+13181
 ;
 const char* commithash = 
-"3e1bf86158af6f8e875e46c8953b00ceb6e40abe";
+"cc4a42d2183fe17b3fafeab93eddfbd4f30855b4";
 const char* revlog = 
+"13181 2017-12-08 15:28:33 +0100 keyboard: DE layout support\n" \
+"13180 2017-12-08 14:56:29 +0100 keyboard: alt, ctrl and layout added\n" \
+"13179 2017-12-08 11:55:09 +0100 cardlg: keyboard support added\n" \
+"13178 2017-12-08 11:29:24 +0100 keyboarddlg: put focus back after ok/cancel\n" \
+"13177 2017-12-08 09:57:09 +0100 toolbardlg: fix for print option\n" \
+"13176 2017-12-08 09:17:27 +0100 planpanel: use soft keyboard for asking item ID in case context on dclick is active\n" \
+"13175 2017-12-08 08:40:46 +0100 rocrailinidialog: keyboard support (wip)\n" \
+"13174 2017-12-07 21:15:16 +0100 connectiondialog: keyboard support added\n" \
+"13173 2017-12-07 19:41:50 +0100 guiapp: fix for crash setting statis text out of event context\n" \
+"13172 2017-12-07 19:09:32 +0100 blockdialog, locdialog: reset last focus pointer on page change\n" \
+"13171 2017-12-07 17:09:40 +0100 locdialog: keyboard support (wip)\n" \
+"13170 2017-12-07 14:20:46 +0100 item: always take over the loco blockenterside on update (TEST)\n" \
+"13169 2017-12-07 13:51:37 +0100 stage: check manual signal if it should be reset\n" \
+"13168 2017-12-07 13:39:36 +0100 toolbardlg: forum item added\n" \
+"13167 2017-12-07 13:11:20 +0100 blockdialog: keyboard support\n" \
+"13166 2017-12-07 12:33:31 +0100 keyboarddlg: layout correction -> US\n" \
+"13165 2017-12-07 11:55:17 +0100 keyboarddlg: wip\n" \
+"13164 2017-12-07 09:58:58 +0100 keyboard for touch: wip\n" \
 "13163 2017-12-06 18:07:18 +0100 loconet lbtcp: recovery fix\n" \
 "13162 2017-12-06 17:38:50 +0100 rocview: context menu on dclick option added for touch\n" \
 "13161 2017-12-06 13:46:22 +0100 item: open/close stage exit on dclick\n" \
@@ -36,23 +54,5 @@ const char* revlog =
 "13134 2017-12-03 12:41:01 +0100 RocControl desktop shortcut added\n" \
 "13133 2017-12-03 12:28:58 +0100 rocctrl: automenu extended\n" \
 "13132 2017-12-03 11:18:18 +0100 rocctrl: wip\n" \
-"13131 2017-12-03 09:00:57 +0100 rocctrl: WIP\n" \
-"13130 2017-12-02 22:37:29 +0100 rocctrl: WIP\n" \
-"13129 2017-12-02 18:42:06 +0100 rocctrl: create read handler first after connect\n" \
-"13128 2017-12-02 18:31:21 +0100 rocctrl: (WIP) a small Python utility for a small display\n" \
-"13127 2017-12-01 16:16:14 +0100 rocview: extra status info at startup\n" \
-"13126 2017-12-01 11:30:42 +0100 loc: fix for function timers > 27\n" \
-"13125 2017-11-30 22:09:48 +0100 app: -pwr command line option added\n" \
-"13124 2017-11-30 22:07:35 +0100 app: -pwr command line option added\n" \
-"13123 2017-11-30 14:20:35 +0100 fback: not delete command node in case of redirection to the controller\n" \
-"13122 2017-11-30 08:02:19 +0100 mvtrack: reset by xmlscript\n" \
-"13121 2017-11-29 22:06:19 +0100 mvtrack: toString implemented\n" \
-"13120 2017-11-29 18:55:46 +0100 disable properties in auto mode for stage too\n" \
-"13119 2017-11-29 16:13:33 +0100 InnoSetup: do not popup if the destination directory already exist\n" \
-"13118 2017-11-29 14:32:45 +0100 InnoSetup: do not popup if the destination directory already exist\n" \
-"13117 2017-11-29 14:22:46 +0100 innosetup: do not use the previous install directory automatically\n" \
-"13116 2017-11-29 12:52:27 +0100 item: fix for invalid locoID pointer\n" \
-"13115 2017-11-29 11:51:24 +0100 mem: isValid function added\n" \
-"13114 2017-11-28 17:23:59 +0100 item: try fix locoid mystery...\n" \
 
 "log end";
