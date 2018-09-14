f36f75a14534a54609caefad18b407c474b007e2
GBert <info@gerhard-bertelsmann.de>
2018-09-14 17:52:37 +0200

    bump revision

--- rocrail/common/version.h
+++ rocrail/common/version.h
@@ -1,60 +1,60 @@
 const int revisionnr = 
-100
+14350
 ;
 const char* commithash = 
 "d6df55e8ce581a398736b07b532495ce29487ae6";
 const char* origin = "LOCAL"
 ;
 const char* revlog = 
-"100 2018-09-14 15:51:15 +0200 renamedlg: clear IIDs from the comboboxes in case of other selection then IID\n" \
-"99 2018-09-14 14:52:46 +0200 massoth: trace loco command in case of getting a slot\n" \
-"98 2018-09-14 12:20:18 +0200 renamedlg: bus field added\n" \
-"97 2018-09-13 13:56:15 +0200 rocweb: bridge sensor/plus symbol corrections\n" \
-"96 2018-09-13 13:28:30 +0200 rocweb: tt sensor correction\n" \
-"95 2018-09-13 11:56:43 +0200 rocweb: SLIM option added for rendering the TT to fit to the SLIM theme\n" \
-"94 2018-09-13 11:05:08 +0200 translation merge: fr\n" \
-"93 2018-09-13 10:13:23 +0200 renderer: function for rotationg only one point for orientation\n" \
-"92 2018-09-13 10:04:25 +0200 rocweb: rotate tt gridcon point to match orientation\n" \
-"91 2018-09-13 08:40:29 +0200 rocweb: render turntable gridcon\n" \
-"90 2018-09-13 08:13:12 +0200 mttmfcc: purge only if all functions are off too\n" \
-"89 2018-09-12 15:32:13 +0200 renderer: simplify rotating\n" \
-"88 2018-09-12 15:10:04 +0200 renderer: simplification of roatating points\n" \
-"87 2018-09-12 09:58:41 +0200 renderer: rotate tt track with a sub function\n" \
-"86 2018-09-12 08:34:10 +0200 renderer: some fine tuning for calculating the tt bridge plus side\n" \
-"85 2018-09-12 08:12:39 +0200 renderer: use cos(-1) as pi\n" \
-"84 2018-09-12 07:53:53 +0200 renderer: fix for drawing the tt bridge plus side in case of graphicalcontext\n" \
-"83 2018-09-11 21:07:07 +0200 virtual: return lenz standard decoder ID for CV7 and 8\n" \
-"82 2018-09-11 15:03:19 +0200 xmlscriptdlg: save anyway option added in case xml is not well formed\n" \
-"81 2018-09-11 12:09:31 +0200 rocweb: show the tt bridge plus side\n" \
-"80 2018-09-11 10:17:09 +0200 mttmfcc: trash reply buffer in case > 256\n" \
-"79 2018-09-11 10:14:56 +0200 mttmfcc: exception in case numer of reply bytes are more then 256\n" \
-"78 2018-09-11 10:03:15 +0200 mttmfcc: function 17-32 implemented for firmware 1.13\n" \
-"77 2018-09-11 08:05:12 +0200 mttmfcc: purge option added\n" \
-"76 2018-09-10 14:46:54 +0200 renderer: show the tt bridge plus side as circle instead of a square\n" \
-"75 2018-09-10 08:21:47 +0200 route: trace level correction for setting crossing block signals\n" \
-"74 2018-09-10 08:02:38 +0200 routedialog: reactiveted the crossingblocksignals option\n" \
-"73 2018-09-09 10:53:01 +0200 rocweb: default tt bridgepos corrected\n" \
-"72 2018-09-09 10:43:59 +0200 rocweb: fix for missing tt symbol size\n" \
-"71 2018-09-09 10:09:26 +0200 mttmfcc: slot purger added\n" \
-"70 2018-09-09 08:59:04 +0200 translation merge: nl, de, sl\n" \
-"69 2018-09-08 15:34:44 +0200 block: inform loco in case of delete\n" \
-"68 2018-09-08 10:36:59 +0200 planpanel: prevent overwriting the add item node with mouse re-enter\n" \
-"67 2018-09-08 08:39:00 +0200 basedlg: check if the dialog is in a visible screen position to restore, if not the x,y will be reset to 10,10\n" \
-"66 2018-09-07 19:30:03 +0200 inno setup: theme paths corrections\n" \
-"65 2018-09-07 16:34:14 +0200 moved SpDrS60-extra to the user themes\n" \
-"64 2018-09-07 16:20:30 +0200 inno setup: typo correction\n" \
-"63 2018-09-07 15:58:57 +0200 inno setup: user themes option added\n" \
-"62 2018-09-07 15:55:56 +0200 userthemes\n" \
-"61 2018-09-07 14:34:03 +0200 themes: removed basic and iltis\n" \
-"60 2018-09-07 14:27:35 +0200 rocguiinidlg: layout correction\n" \
-"59 2018-09-07 10:58:32 +0200 trackpickerdlg: removed roads threeway\n" \
-"58 2018-09-07 10:38:16 +0200 renderer: disable show IDs for road symbols\n" \
-"57 2018-09-07 10:12:59 +0200 roads: added ccrossing and road/track option in the selectiondlg\n" \
-"56 2018-09-07 08:57:45 +0200 roclcdr: fix for reenter in case of half automatic mode\n" \
-"55 2018-09-07 08:11:06 +0200 trackpickerdlg: added road short block\n" \
-"54 2018-09-07 07:58:28 +0200 Roads: fixes for small block symbol and occ color\n" \
-"53 2018-09-07 07:49:56 +0200 rocrailinidialog: moved the 'half auto stop at in' to the loco section\n" \
-"52 2018-09-06 14:19:46 +0200 roads theme: clean up and make compatible with the standard theme\n" \
-"51 2018-09-06 11:47:29 +0200 rocguiinidlg: minimal toolbar icon size 24\n" \
+"14350 2018-09-14 15:51:15 +0200 renamedlg: clear IIDs from the comboboxes in case of other selection then IID\n" \
+"14349 2018-09-14 14:52:46 +0200 massoth: trace loco command in case of getting a slot\n" \
+"14348 2018-09-14 12:20:18 +0200 renamedlg: bus field added\n" \
+"14347 2018-09-13 13:56:15 +0200 rocweb: bridge sensor/plus symbol corrections\n" \
+"14346 2018-09-13 13:28:30 +0200 rocweb: tt sensor correction\n" \
+"14345 2018-09-13 11:56:43 +0200 rocweb: SLIM option added for rendering the TT to fit to the SLIM theme\n" \
+"14344 2018-09-13 11:05:08 +0200 translation merge: fr\n" \
+"14343 2018-09-13 10:13:23 +0200 renderer: function for rotationg only one point for orientation\n" \
+"14342 2018-09-13 10:04:25 +0200 rocweb: rotate tt gridcon point to match orientation\n" \
+"14341 2018-09-13 08:40:29 +0200 rocweb: render turntable gridcon\n" \
+"14340 2018-09-13 08:13:12 +0200 mttmfcc: purge only if all functions are off too\n" \
+"14339 2018-09-12 15:32:13 +0200 renderer: simplify rotating\n" \
+"14338 2018-09-12 15:10:04 +0200 renderer: simplification of roatating points\n" \
+"14337 2018-09-12 09:58:41 +0200 renderer: rotate tt track with a sub function\n" \
+"14336 2018-09-12 08:34:10 +0200 renderer: some fine tuning for calculating the tt bridge plus side\n" \
+"14335 2018-09-12 08:12:39 +0200 renderer: use cos(-1) as pi\n" \
+"14334 2018-09-12 07:53:53 +0200 renderer: fix for drawing the tt bridge plus side in case of graphicalcontext\n" \
+"14333 2018-09-11 21:07:07 +0200 virtual: return lenz standard decoder ID for CV7 and 8\n" \
+"14332 2018-09-11 15:03:19 +0200 xmlscriptdlg: save anyway option added in case xml is not well formed\n" \
+"14331 2018-09-11 12:09:31 +0200 rocweb: show the tt bridge plus side\n" \
+"14330 2018-09-11 10:17:09 +0200 mttmfcc: trash reply buffer in case > 256\n" \
+"14329 2018-09-11 10:14:56 +0200 mttmfcc: exception in case numer of reply bytes are more then 256\n" \
+"14328 2018-09-11 10:03:15 +0200 mttmfcc: function 17-32 implemented for firmware 1.13\n" \
+"14327 2018-09-11 08:05:12 +0200 mttmfcc: purge option added\n" \
+"14326 2018-09-10 14:46:54 +0200 renderer: show the tt bridge plus side as circle instead of a square\n" \
+"14325 2018-09-10 08:21:47 +0200 route: trace level correction for setting crossing block signals\n" \
+"14324 2018-09-10 08:02:38 +0200 routedialog: reactiveted the crossingblocksignals option\n" \
+"14323 2018-09-09 10:53:01 +0200 rocweb: default tt bridgepos corrected\n" \
+"14322 2018-09-09 10:43:59 +0200 rocweb: fix for missing tt symbol size\n" \
+"14321 2018-09-09 10:09:26 +0200 mttmfcc: slot purger added\n" \
+"14320 2018-09-09 08:59:04 +0200 translation merge: nl, de, sl\n" \
+"14319 2018-09-08 15:34:44 +0200 block: inform loco in case of delete\n" \
+"14318 2018-09-08 10:36:59 +0200 planpanel: prevent overwriting the add item node with mouse re-enter\n" \
+"14317 2018-09-08 08:39:00 +0200 basedlg: check if the dialog is in a visible screen position to restore, if not the x,y will be reset to 10,10\n" \
+"14316 2018-09-07 19:30:03 +0200 inno setup: theme paths corrections\n" \
+"14315 2018-09-07 16:34:14 +0200 moved SpDrS60-extra to the user themes\n" \
+"14314 2018-09-07 16:20:30 +0200 inno setup: typo correction\n" \
+"14313 2018-09-07 15:58:57 +0200 inno setup: user themes option added\n" \
+"14312 2018-09-07 15:55:56 +0200 userthemes\n" \
+"14311 2018-09-07 14:34:03 +0200 themes: removed basic and iltis\n" \
+"14310 2018-09-07 14:27:35 +0200 rocguiinidlg: layout correction\n" \
+"14309 2018-09-07 10:58:32 +0200 trackpickerdlg: removed roads threeway\n" \
+"14308 2018-09-07 10:38:16 +0200 renderer: disable show IDs for road symbols\n" \
+"14307 2018-09-07 10:12:59 +0200 roads: added ccrossing and road/track option in the selectiondlg\n" \
+"14306 2018-09-07 08:57:45 +0200 roclcdr: fix for reenter in case of half automatic mode\n" \
+"14305 2018-09-07 08:11:06 +0200 trackpickerdlg: added road short block\n" \
+"14304 2018-09-07 07:58:28 +0200 Roads: fixes for small block symbol and occ color\n" \
+"14303 2018-09-07 07:49:56 +0200 rocrailinidialog: moved the 'half auto stop at in' to the loco section\n" \
+"14302 2018-09-06 14:19:46 +0200 roads theme: clean up and make compatible with the standard theme\n" \
+"14301 2018-09-06 11:47:29 +0200 rocguiinidlg: minimal toolbar icon size 24\n" \
 
 "log end";
