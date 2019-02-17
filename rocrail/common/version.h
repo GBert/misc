e4f82eeee68de389b946fd139b011e8d99d1a7da
GBert <info@gerhard-bertelsmann.de>
2019-02-17 16:53:38 +0100

    bump revision

--- rocrail/common/version.h
+++ rocrail/common/version.h
@@ -1,11 +1,13 @@
 const int revisionnr = 
-15059
+15061
 ;
 const char* commithash = 
-"e3dd3110109ae7d9fa4f44c89d31f234e8c029e1";
+"78110512c8565ce92ce7c3ce38c8b4215eae50ac";
 const char* origin = "LOCAL"
 ;
 const char* revlog = 
+"15061 2019-02-17 16:10:44 +0100 mbus: removed initial sensor query :|\n" \
+"15060 2019-02-17 16:03:49 +0100 mbus: tcp trace fix for port number\n" \
 "15059 2019-02-17 15:55:13 +0100 mbus: power on at startup option added\n" \
 "15058 2019-02-17 14:35:13 +0100 mbusdlg: translations and column resizing\n" \
 "15057 2019-02-17 13:07:38 +0100 mbus: only SoD sensors if the number of connected modules > 0\n" \
@@ -2004,7 +2006,5 @@ const char* revlog =
 "13064 2017-11-20 20:05:24 +0100 rpic: wip\n" \
 "13063 2017-11-20 19:21:20 +0100 rpic: wip\n" \
 "13062 2017-11-20 18:15:59 +0100 rpic: wip\n" \
-"13061 2017-11-20 17:04:46 +0100 guiapp: update route in case not visable\n" \
-"13060 2017-11-20 16:34:23 +0100 innosetup: set support for 64bit only in case of 64bit build\n" \
 
 "log end";
