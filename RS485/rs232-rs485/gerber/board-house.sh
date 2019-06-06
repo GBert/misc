4a83bee5445674842a6a80c39ac19678fb6647da
cb2begq <info@gerhard-bertelsmann.de>
2019-06-06 18:53:37 +0200

    GML -> GM1

--- RS485/rs232-rs485/gerber/board-house.sh
+++ RS485/rs232-rs485/gerber/board-house.sh
@@ -10,13 +10,13 @@ cp ${BOARD}-B_Mask.gbs    ${BOARD}.GBS
 cp ${BOARD}-F_SilkS.gto   ${BOARD}.GTO
 cp ${BOARD}-B_SilkS.gbo   ${BOARD}.GBO
 cp ${BOARD}-PTH.drl           ${BOARD}-PTH.TXT
-cp ${BOARD}-Edge_Cuts.gm1 ${BOARD}.GML
+cp ${BOARD}-Edge_Cuts.gm1 ${BOARD}.GM1
 
 if [ -f ${BOARD}-NPTH.drl ]; then
     cp ${BOARD}-NPTH.drl      ${BOARD}-NPTH.TXT
     rm -f ${BOARD}.ZIP
-    zip ${BOARD}.ZIP ${BOARD}.GTL ${BOARD}.GBL ${BOARD}.GTS ${BOARD}.GBS ${BOARD}.GTO ${BOARD}.GBO ${BOARD}-PTH.TXT ${BOARD}-NPTH.TXT ${BOARD}.GML 
+    zip ${BOARD}.ZIP ${BOARD}.GTL ${BOARD}.GBL ${BOARD}.GTS ${BOARD}.GBS ${BOARD}.GTO ${BOARD}.GBO ${BOARD}-PTH.TXT ${BOARD}-NPTH.TXT ${BOARD}.GM1 
 else
     rm -f ${BOARD}.ZIP
-    zip ${BOARD}.ZIP ${BOARD}.GTL ${BOARD}.GBL ${BOARD}.GTS ${BOARD}.GBS ${BOARD}.GTO ${BOARD}.GBO ${BOARD}-PTH.TXT ${BOARD}.GML 
+    zip ${BOARD}.ZIP ${BOARD}.GTL ${BOARD}.GBL ${BOARD}.GTS ${BOARD}.GBS ${BOARD}.GTO ${BOARD}.GBO ${BOARD}-PTH.TXT ${BOARD}.GM1 
 fi
