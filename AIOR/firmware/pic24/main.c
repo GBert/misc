ae7892fb4ba960c50c346687db12154023d8dd68
GBert <info@gerhard-bertelsmann.de>
2019-06-23 16:47:02 +0200

    wip

--- AIOR/firmware/pic24/main.c
+++ AIOR/firmware/pic24/main.c
@@ -16,6 +16,20 @@
 #pragma config OSCIOFNC = ON
 #pragma config POSCMD   = XT
 
+volatile uint16_t LED_Counter = 0;
+
+
+/* code for Output Compare 1 ISR*/
+void __attribute__((__interrupt__, no_auto_psv)) _T1Interrupt( void ) {
+    /* Clear OC1 interrupt flag} */
+    IFS0bits.T1IF = 0;
+    LED_Counter++;
+    if (LED_Counter >= 500) {
+	__builtin_btg((unsigned int *)&LATB, 9);
+	LED_Counter = 0;
+    }
+}
+
 /* code for Output Compare 1 ISR*/
 void __attribute__((__interrupt__, no_auto_psv)) _OC1Interrupt( void ) {
     /* Clear OC1 interrupt flag} */
@@ -26,6 +40,20 @@ void __attribute__((__interrupt__, no_auto_psv)) _OC1Interrupt( void ) {
 void __attribute__((__interrupt__, no_auto_psv)) _OC2Interrupt( void ) {
     /* Clear OC2 interrupt flag} */
     IFS0bits.OC2IF = 0;
+}
+
+void init_timer(void) {
+	/* Timer1 for msic tasks */
+	T1CONbits.TON = 0;        // Disable Timer
+	T1CONbits.TCS = 0;        // Select internal instruction cycle clock
+	T1CONbits.TGATE = 0;      // Disable Gated Timer mode
+	T1CONbits.TCKPS = 0b10;   // Select 1:256 Prescaler
+	TMR1 = 0x00;              // Clear timer register
+	PR1 = 999;                // Load the period value
+	IPC0bits.T1IP = 0x01;     // Set Timer1 Interrupt Priority Level
+	IFS0bits.T1IF = 0;        // Clear Timer1 Interrupt Flag
+	IEC0bits.T1IE = 1;        // Enable Timer1 interrupt
+	T1CONbits.TON = 1;        // Start Timer
 }
 
 void init_io(void) {
@@ -53,10 +81,10 @@ void init_pps(void) {
     RPOR6bits.RP54R   = 14;     /* PIN 2  RC6 RP54 CAN1 TxD (C1TX = 0b001110) */
     RPINR19bits.U2RXR = 42;     /* PIN 8  RB10 RP42 U2RxD */
     RPOR4bits.RP42R   =  3;     /* PIN 9  RB11 RP43 U2TxD (U2TX = 0b0000011) */
-    RPOR2bits.RP39R   = 16;	/* PIN 43 RB7  RP39 B_Left  (OC1 = 0b00010000) */
-    RPOR3bits.RP40R   = 17;	/* PIN 44 RB8  RP40 B_Right (OC2 = 0b00010001) */
-				/* PIN 42 RB6  RP38 B_Ena */
-   __builtin_write_OSCCONL(OSCCON | (1 << 6));
+    RPOR2bits.RP39R   = 16;     /* PIN 43 RB7  RP39 B_Left  (OC1 = 0b00010000) */
+    RPOR3bits.RP40R   = 17;     /* PIN 44 RB8  RP40 B_Right (OC2 = 0b00010001) */
+                                /* PIN 42 RB6  RP38 B_Ena */
+    __builtin_write_OSCCONL(OSCCON | (1 << 6));
 }
 
 int main(void) {
@@ -72,9 +100,9 @@ int main(void) {
     while (OSCCONbits.LOCK != 1) ;
 
     init_io();
+    init_timer();
 
     while (true) {
 	__delay_ms(500);
-	__builtin_btg((unsigned int *)&LATB, 9);
     }
 }
