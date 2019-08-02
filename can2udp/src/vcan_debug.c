9e5df17aaac8e7a6186d6bb6cb780debfaa18f48
cb2begq <info@gerhard-bertelsmann.de>
2019-08-02 17:44:27 +0200

    wip

--- can2udp/src/vcan_debug.c
+++ can2udp/src/vcan_debug.c
@@ -20,20 +20,22 @@
 
 #define MAXDG   	4096	/* maximum datagram size */
 #define MAXUDP  	16	/* maximum datagram size */
+#define TDEFAULT	100
 #define MAX(a,b)	((a) > (b) ? (a) : (b))
 
-static char *F_CAN_FORMAT_STRG   = "      CAN->  CANID 0x%08X R [%d]";
-static char *T_CAN_FORMAT_STRG   = "      CAN<-  CANID 0x%08X   [%d]";
+static char *F_CAN_FORMAT_STRG = "      CAN->  CANID 0x%08X R [%d]";
+static char *T_CAN_FORMAT_STRG = "      CAN<-  CANID 0x%08X   [%d]";
 
 unsigned char netframe[MAXDG];
 
-static unsigned char M_DEBUG[] =      { 0x00, 0x64, 0x03, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static unsigned char M_DEBUG[] = { 0x00, 0x64, 0x03, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
 void print_usage(char *prg) {
     fprintf(stderr, "\nUsage: %s -i <can interface>\n", prg);
     fprintf(stderr, "   Version 0.9\n\n");
     fprintf(stderr, "         -i <can int>        can interface - default vcan1\n");
-    fprintf(stderr, "         -d                  daemonize\n\n");
+    fprintf(stderr, "         -t <timeout>        time gap between frames in [ms] - default %d\n", TDEFAULT);
+    fprintf(stderr, "\n\n");
 }
 
 int time_stamp(char *timestamp) {
@@ -84,48 +86,51 @@ int send_can_frame(int can_socket, struct can_frame *frame, int verbose) {
     return 0;
 }
 
-int send_defined_can_frame(int can_socket, unsigned char *data, int verbose) {
+int send_randomized_can_frame(int can_socket, unsigned char *data, int verbose) {
     struct can_frame frame;
-    uint32_t can_id;
+    uint32_t i, can_id;
     memset(&frame, 0, sizeof(frame));
     memcpy(&can_id, &data[0], 4);
     frame.can_id = htonl(can_id);
     frame.can_dlc = data[4];
-    memcpy(&frame.data, &data[5], 8);
+    /* memcpy(&frame.data, &data[5], 8); */
+
+    for (i = 0; i < sizeof(frame.data); i++)
+	frame.data[i] = (uint8_t) rand();
+
     send_can_frame(can_socket, &frame, verbose);
-    return 0;
-}
-
-int send_can_block(int can_socket, unsigned char data[][13], unsigned int rows, int verbose) {
-    unsigned int i;
-
-    for (i = 0; i < rows; i++)
-	send_defined_can_frame(can_socket, data[i], verbose);
-
     return 0;
 }
 
 int main(int argc, char **argv) {
     int max_fds, opt;
     struct can_frame frame_q, frame_r;
+    unsigned int tmsec;
     struct timeval timeout;
 
-    int sc;
+    int s, sc;
     struct sockaddr_can caddr;
     struct ifreq ifr;
     socklen_t caddrlen = sizeof(caddr);
 
     fd_set read_fds;
 
-    int verbose = 1;
+    int verbose = 0;
+    tmsec = TDEFAULT;
     strcpy(ifr.ifr_name, "vcan1");
     memset(&timeout, 0, sizeof(timeout));
-    timeout.tv_usec = 50000;
+    timeout.tv_usec = tmsec * 1000;
 
-    while ((opt = getopt(argc, argv, "i:dh?")) != -1) {
+    while ((opt = getopt(argc, argv, "i:t:vh?")) != -1) {
 	switch (opt) {
 	case 'i':
 	    strncpy(ifr.ifr_name, optarg, sizeof(ifr.ifr_name) - 1);
+	    break;
+	case 't':
+	    tmsec = strtoul(optarg, (char **)NULL, 10);
+	    break;
+	case 'v':
+	    verbose = 1;
 	    break;
 	case 'h':
 	case '?':
@@ -163,26 +168,32 @@ int main(int argc, char **argv) {
     max_fds = sc;
 
     while (1) {
-	if (select(max_fds + 1, &read_fds, NULL, NULL, NULL) < 0) {
+	s = select(max_fds + 1, &read_fds, NULL, NULL, &timeout);
+	if (s < 0) {
 	    fprintf(stderr, "select error: %s\n", strerror(errno));
 	    exit(EXIT_FAILURE);
-	}
-	/* received a CAN frame */
-	if (FD_ISSET(sc, &read_fds)) {
-	    if (read(sc, &frame_r, sizeof(struct can_frame)) < 0) {
-		fprintf(stderr, "error reading CAN frame: %s\n", strerror(errno));
-	    } else if (frame_r.can_id & CAN_EFF_FLAG) {	/* only EFF frames are valid */
-		if (verbose) {
-		    print_can_frame(F_CAN_FORMAT_STRG, &frame_q);
-		}
+	} else if (s == 0) {
+	    send_randomized_can_frame(sc, M_DEBUG, verbose);
+	    timeout.tv_sec = 0;
+	    timeout.tv_usec = tmsec * 1000;
+	} else {
+	    /* received a CAN frame */
+	    if (FD_ISSET(sc, &read_fds)) {
+		if (read(sc, &frame_r, sizeof(struct can_frame)) < 0) {
+		    fprintf(stderr, "error reading CAN frame: %s\n", strerror(errno));
+		} else if (frame_r.can_id & CAN_EFF_FLAG) {	/* only EFF frames are valid */
+		    if (verbose) {
+			print_can_frame(F_CAN_FORMAT_STRG, &frame_q);
+		    }
 
-		switch ((frame.can_id & 0x00FF0000UL) >> 16) {
-		case 0x65:
-		    print_can_frame(F_CAN_FORMAT_STRG, &frame_q);
-		    print_can_frame(F_CAN_FORMAT_STRG, &frame_r);
-		    break;
-		default:
-		    break;
+		    switch ((frame_r.can_id & 0x00FF0000UL) >> 16) {
+		    case 0x65:
+			print_can_frame(F_CAN_FORMAT_STRG, &frame_q);
+			print_can_frame(F_CAN_FORMAT_STRG, &frame_r);
+			break;
+		    default:
+			break;
+		    }
 		}
 	    }
 	}
