23eed77adc7333044b89c088f47f13b6c40d4651
GBert <info@gerhard-bertelsmann.de>
2019-08-31 14:39:49 +0200

    bump revision

--- rocrail/common/version.h
+++ rocrail/common/version.h
@@ -1,11 +1,12 @@
 const int revisionnr = 
-15930
+15931
 ;
 const char* commithash = 
-"5fd67c67a16d3e95317e816c537e14a2c72a224d";
+"c790aeb8034930e104ef1fa6b7251a4b560c6b9d";
 const char* origin = "LOCAL"
 ;
 const char* revlog = 
+"15931 2019-08-31 07:40:29 +0200 rocdigs: makefile clear rascii dir\n" \
 "15930 2019-08-30 08:07:03 +0200 demon plan: text pointsize correction\n" \
 "15929 2019-08-29 08:24:49 +0200 guiapp: added more language selections\n" \
 "15928 2019-08-28 15:59:31 +0200 infodialog: revert smalles font size for disclaimer\n" \
@@ -2005,6 +2006,5 @@ const char* revlog =
 "13934 2018-05-05 08:28:06 +0200 locationsdlg: sort schedule color list\n" \
 "13933 2018-05-05 08:19:48 +0200 locationsdialog: show schedule colors on the graph tab\n" \
 "13932 2018-05-05 07:41:11 +0200 loc: inform slave locos in case of a pause command\n" \
-"13931 2018-05-05 06:57:47 +0200 ecos: fix for address offset in case of RailCom\n" \
 
 "log end";
