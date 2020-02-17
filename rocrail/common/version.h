467f2c0d154683edd924628616111d2f5ebc614f
GBert <info@gerhard-bertelsmann.de>
2020-02-17 12:21:47 +0100

    bump revision

--- rocrail/common/version.h
+++ rocrail/common/version.h
@@ -1,11 +1,12 @@
 const int revisionnr = 
-483
+484
 ;
 const char* commithash = 
-"0cb0cfc80fd4c7329efa17cead929e02cceaffbf";
+"85bec2da309c26c1b57edf55e8634317fdf9105c";
 const char* origin = "LOCAL"
 ;
 const char* revlog = 
+"484 2020-02-17 08:11:05 +0100 item: show loco ID in the popup title too\n" \
 "483 2020-02-16 10:16:57 +0100 xnet: check for version 3.8+ in case of request POM read data from CS\n" \
 "482 2020-02-16 09:18:00 +0100 xnet: lzv200 POM read\n" \
 "481 2020-02-15 11:17:32 +0100 rascii: update\n" \
