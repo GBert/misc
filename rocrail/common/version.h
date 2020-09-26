025ef75625935bfb64f4fa45689f89eaaf3de339
GBert <info@gerhard-bertelsmann.de>
2020-09-26 16:25:03 +0200

    bump revision

--- rocrail/common/version.h
+++ rocrail/common/version.h
@@ -1,11 +1,13 @@
 const int revisionnr = 
-1004
+1006
 ;
 const char* commithash = 
-"e7a0909f82241348aa27884e2ae3cd226ef5e52a";
+"32b68bee3c7c55ff7570524f555c6e8ce58e8026";
 const char* origin = "LOCAL"
 ;
 const char* revlog = 
+"1006 2020-09-25 21:53:23 +0200 model: regard only active locos in case of checking restored\n" \
+"1005 2020-09-25 10:36:50 +0200 model: check if all locos did restore before starting the auto mode (optional, default off)\n" \
 "1004 2020-09-22 08:47:06 +0200 ahome: report all event childs\n" \
 "1003 2020-09-22 08:13:01 +0200 wio: stop loco in case of empty battery\n" \
 "1002 2020-09-19 14:55:44 +0200 tt: compiler fix for leopard\n" \
