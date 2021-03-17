e44d72d98003acecbe4d9495b88156f875aab2ef
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2021-03-17 09:10:41 +0100

    fix TURNOUT_INFO

--- z21/src/z21emu.c
+++ z21/src/z21emu.c
@@ -86,7 +86,7 @@ static unsigned char XPN_X_STORE2[]               = { 0x14, 0x00, 0x16, 0x00, 0x
 
 void print_usage(char *prg) {
     fprintf(stderr, "\nUsage: %s -c config_dir -p <port> -s <port>\n", prg);
-    fprintf(stderr, "   Version 1.11\n\n");
+    fprintf(stderr, "   Version 1.12\n\n");
     fprintf(stderr, "         -c <config_dir>     set the config directory - default %s\n", config_dir);
     fprintf(stderr, "         -p <port>           primary UDP port for the server - default %d\n", PRIMARY_UDP_PORT);
     fprintf(stderr, "         -s <port>           secondary UDP port for the server - default %d\n", SECONDARY_UDP_PORT);
@@ -731,6 +731,10 @@ int check_data_can(struct z21_data_t *z21_data, uint8_t * data, int verbose) {
     case 0x17:
 	/* TODO */
 	uid = be16(&data[7]) & 0xCFFF;
+	if (uid > 0x37FF)
+	    uid -= 0x3800;
+	if (uid > 0x2FFF)
+	    uid -= 0x3000;
 	tport = 1 << (data[9] & 0x01);
 	tpower = data[10];
 	v_printf(verbose, "\n");
