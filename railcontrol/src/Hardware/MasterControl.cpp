72d64d6a648a5a70199337078dd92c3e9ae701f8
GBert <info@gerhard-bertelsmann.de>
2021-05-12 09:44:34 +0200

    add missing files

---
+++ railcontrol/src/Hardware/MasterControl.cpp
@@ -0,0 +1,61 @@
+/*
+RailControl - Model Railway Control Software
+
+Copyright (c) 2017-2021 Dominik (Teddy) Mahrer - www.railcontrol.org
+
+RailControl is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3, or (at your option) any
+later version.
+
+RailControl is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RailControl; see the file LICENCE. If not see
+<http://www.gnu.org/licenses/>.
+*/
+
+#include "Hardware/MasterControl.h"
+#include "Languages.h"
+#include "Utils/Utils.h"
+
+namespace Hardware
+{
+	MasterControl::MasterControl(const HardwareParams* params)
+	:	SerialP50x(params, "MasterControl")
+	{
+		SendP50XOnly();
+		const bool ok = SendNop();
+		if (!ok)
+		{
+			logger->Error(Languages::TextControlDoesNotAnswer);
+			return;
+		}
+
+		s88Modules = Utils::Utils::StringToInteger(params->GetArg2(), 0);
+
+		if (s88Modules > MaxS88ModulesMasterControl)
+		{
+			logger->Error(Languages::TextTooManyS88Modules, s88Modules, MaxS88ModulesMasterControl);
+			return;
+		}
+
+		if (s88Modules == 0)
+		{
+			logger->Info(Languages::TextNoS88Modules);
+			return;
+		}
+
+		logger->Info(Languages::TextNrOfS88Modules, s88Modules);
+		unsigned char modules = SendXP88Get(0);
+		if (modules != s88Modules)
+		{
+			SendXP88Set(0, s88Modules);
+		}
+
+		checkEventsThread = std::thread(&Hardware::ProtocolP50x::CheckEventsWorker, this);
+	}
+} // namespace
---
+++ railcontrol/src/Hardware/MasterControl.h
@@ -0,0 +1,56 @@
+/*
+RailControl - Model Railway Control Software
+
+Copyright (c) 2017-2021 Dominik (Teddy) Mahrer - www.railcontrol.org
+
+RailControl is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3, or (at your option) any
+later version.
+
+RailControl is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RailControl; see the file LICENCE. If not see
+<http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <string>
+
+#include "Hardware/SerialP50x.h"
+#include "Languages.h"
+
+namespace Hardware
+{
+	class HardwareParams;
+
+	class MasterControl : public SerialP50x
+	{
+		public:
+			MasterControl() = delete;
+			MasterControl(const MasterControl&) = delete;
+			MasterControl& operator=(const MasterControl&) = delete;
+
+			MasterControl(const HardwareParams* params);
+
+			virtual ~MasterControl()
+			{
+			}
+
+			static inline void GetArgumentTypesAndHint(std::map<unsigned char,ArgumentType>& argumentTypes, std::string& hint)
+			{
+				argumentTypes[1] = ArgumentTypeSerialPort;
+				argumentTypes[2] = ArgumentTypeS88Modules;
+				hint = Languages::GetText(Languages::TextHintMasterControl);
+			}
+
+		private:
+			static const unsigned char MaxS88ModulesMasterControl = 104;
+	};
+} // namespace
+
